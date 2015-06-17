fb30f5fdfa82dff98702dabdb039db771b7a3bd8
Hugh <2252327467@qq.com>
2015-06-17 14:46:32 +0800

    jackson

--- ef-model/src/main/java/com/efeiyi/ec/organization/model/User.java
+++ ef-model/src/main/java/com/efeiyi/ec/organization/model/User.java
@@ -1,8 +1,9 @@
 package com.efeiyi.ec.organization.model;
 
 
-import org.codehaus.jackson.annotate.JsonIgnore;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
