7763117a6db508811f38045222126d76517134d1
STUBBORNCOBOY <754767681@qq.com>
2015-06-19 10:21:04 +0800

    system开发

---
+++ ef-system/src/main/src/com/efeiyi/ec/system/organization/util/AuthorizationUtil.java
@@ -0,0 +1,58 @@
+package com.efeiyi.ec.system.organization.util;
+
+
+
+import com.efeiyi.ec.website.organization.model.MyUser;
+import com.efeiyi.ec.website.organization.model.Role;
+import com.efeiyi.ec.website.organization.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
+/**
+ * @author WuYingbo
+ */
+public class AuthorizationUtil {
+
+
+    public static MyUser getMyUser() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        try {
+            return (MyUser) authentication.getPrincipal();
+        } catch (Exception e) {
+            MyUser myUser = new MyUser();
+            Role role = new Role();
+            role.setBasicType("all");
+
+            myUser.setRole(role);
+            return myUser;
+
+
+        }
+    }
+
+
+    public static User getUser() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        MyUser myUser = (MyUser) authentication.getPrincipal();
+        User user = new User();
+        user.setId(myUser.getId());
+        user.setName(myUser.getName());
+        user.setUsername(myUser.getUsername());
+        return user;
+    }
+
+    public static String getRoleType() {
+        MyUser myUser = getMyUser();
+        if (myUser != null && myUser.getId() != null) {
+            return myUser.getRole().getBasicType();
+        } else {
+            return "";
+        }
+    }
+
+
+}
