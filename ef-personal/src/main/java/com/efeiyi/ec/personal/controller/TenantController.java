b4ff4a8c118283d1e11bc54a1fadacae1a8aa821
wlm123456 <598719390@qq.com>
2015-07-06 16:25:21 +0800

    传承人工坊增加

--- ef-personal/src/main/java/com/efeiyi/ec/personal/controller/TenantController.java
+++ ef-personal/src/main/java/com/efeiyi/ec/personal/controller/TenantController.java
@@ -167,9 +167,30 @@ public class TenantController extends BaseController {
         model.addAttribute("productWorkList", productWorkList);
         return new ModelAndView("/tenant/tenantWork/tenantWorkView", model);
     }
+
     /**
-     * 注释
+     * 获取传承人工作坊
+     * @param model
+     * @return
      */
+    @RequestMapping("/tenantWorkShopList.do")
+    public ModelAndView listTenantWorkShop(HttpServletRequest request ,ModelMap model){
+        String condition = request.getParameter("condition");
+        LinkedHashMap<String, Object> queryParamMap = new LinkedHashMap<>();
+        String tenantId = request.getParameter("tenantId");
+        Tenant tenant = (Tenant)baseManager.getObject(Tenant.class.getName(),tenantId);
+        model.addAttribute("tenant",tenant);
+        StringBuffer sb = new StringBuffer("from TenantWorkShop t where t.tenant.id = :tenantId");
+        queryParamMap.put("tenantId",tenantId);
+
+        if(condition != null && !"".equals(condition) && Long.valueOf(condition) > 0){
+            sb.append(" and YEAR(t.createDateTime) = "+ Integer.valueOf(condition));
+        }
+        sb.append(" order by t.createDateTime DESC");
+        TenantWorkShop productWorkShop = (TenantWorkShop) baseManager.getUniqueObjectByConditions(sb.toString(), queryParamMap);
+        model.addAttribute("productWorkShop", productWorkShop);
+        return new ModelAndView("/tenant/tenantWorkShop/tenantWorkShopView", model);
+    }
 
 
 }
