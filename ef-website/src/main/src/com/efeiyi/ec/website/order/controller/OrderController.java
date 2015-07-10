3303f67f38275baca5544badce8a5c92a06d271c
jiayankun <jiayankun24@foxmail.com>
2015-07-10 10:13:34 +0800

    订单管理

--- ef-website/src/main/src/com/efeiyi/ec/website/order/controller/OrderController.java
+++ ef-website/src/main/src/com/efeiyi/ec/website/order/controller/OrderController.java
@@ -1,4 +1,5 @@
 package com.efeiyi.ec.website.order.controller;
+
 import com.efeiyi.ec.purchase.model.PurchaseOrder;
 import com.ming800.core.base.controller.BaseController;
 import com.ming800.core.base.service.BaseManager;
@@ -6,10 +7,7 @@ import com.ming800.core.does.model.XQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -25,25 +23,33 @@ public class OrderController extends BaseController {
 
     @RequestMapping(value = "/listOrder.do")
     public String queryOrderList(Model model,HttpServletRequest request ) throws Exception{
-//        String userId = request.getParameter("userId");
-//        if(null==userId||"".equals(userId)){
-//            modelMap.put("code", "userId为空");
-//        }else{
-//            //业务处理
-//            String queryHql = "from PurchaseOrder s where s.id = ?";
-//            List<Object> obj = new ArrayList<Object>();
-//            obj.add(userId);
-//            List<PurchaseOrder> list = baseManager.listObject(queryHql,String.valueOf(obj));
-//           /* List<PurchaseOrder> list = baseManager.listObject(queryHql,queryParamMap);*/
-//            modelMap.put("listOrder", list);
-//        }
-//        return new ModelAndView("/ordertest",modelMap);
         XQuery xQuery = new XQuery("listPurchaseOrder_default",request);
-//        List<Object> list = this.baseManager.listObject(xQuery);
         xQuery.addRequestParamToModel(model,request);
         List list = baseManager.listPageInfo(xQuery).getList();
 
         model.addAttribute("list",list);
         return "/pc/purchaseOrder/purchaseOrderList";
     }
+
+//    @RequestMapping(value = "/getOrder.do")
+//    public String getOrder(Model model,HttpServletRequest request ) throws Exception{
+//        XQuery xQuery = new XQuery("viewPurchaseOrder_getOrder",request);
+//        List<Object> list = this.baseManager.listObject(xQuery);
+//
+//        PurchaseOrder purchaseOrder = new PurchaseOrder();
+//        if (list != null && list.size() > 0){
+//            purchaseOrder = (PurchaseOrder) list.get(0);
+//        }
+//
+//        model.addAttribute("order",purchaseOrder);
+//        return "/pc/purchaseOrder/purchaseOrderView";
+//    }
+
+    @RequestMapping(value = "/getOrder.do")
+    public String getOrder(Model model,HttpServletRequest request ) throws Exception{
+        PurchaseOrder purchaseOrder = (PurchaseOrder) baseManager.getObject(PurchaseOrder.class.getName(),request.getParameter("id"));
+        model.addAttribute("order",purchaseOrder);
+        return "/pc/purchaseOrder/purchaseOrderView";
+    }
+
 }
