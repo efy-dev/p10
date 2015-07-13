e40c67bda4daf0aa45f836a76f92ce5a67f5101c
wlm123456 <598719390@qq.com>
2015-07-13 09:35:03 +0800

    品类配置文件提交

---
+++ ef-website/src/main/src/com/efeiyi/ec/website/project/controller/ProjectController.java
@@ -0,0 +1,42 @@
+package com.efeiyi.ec.website.project.controller;
+import com.efeiyi.ec.project.model.Project;
+import com.ming800.core.base.controller.BaseController;
+import com.ming800.core.base.service.BaseManager;
+import com.ming800.core.does.model.XQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+/**
+ * Created by Administrator on 2015/6/25.
+ */
+@Controller
+@RequestMapping("/project")
+public class ProjectController extends BaseController {
+    @Autowired
+    private BaseManager baseManager;
+
+    @RequestMapping(value = "/listProject.do")
+    public String queryProjectList(Model model,HttpServletRequest request ) throws Exception{
+
+        XQuery xQuery = new XQuery("plistProject_default",request);
+        List list = this.baseManager.listObject(xQuery);
+//        xQuery.addRequestParamToModel(model,request);
+//        List list = baseManager.listPageInfo(xQuery).getList();
+
+        model.addAttribute("list",list);
+        return "/pc/project/projectlist";
+    }
+
+    @RequestMapping(value = "/getProject.do")
+    public String getProject(Model model,HttpServletRequest request ) throws Exception{
+        String id = request.getParameter("id");
+        Project project = (Project) this.baseManager.getObject(Project.class.getName(), id);
+        model.addAttribute("project",project);
+        return "/pc/project/projectView";
+    }
+}
