caa87199914e13afcf21804fcd0519271481c86d
kaysonyang <kayson_yang@163.com>
2015-07-15 09:18:08 +0800

    junit使用非注解方式加载初始化bean容器

--- ef-website/src/test/java/com.ming800.core/base/controller/BannerControllerTest.java
+++ ef-website/src/test/java/com.ming800.core/base/controller/BannerControllerTest.java
@@ -1,27 +1,32 @@
 package com.ming800.core.base.controller;
 
 
-import com.ming800.core.base.model.Banner;
+import com.ming800.core.base.service.impl.BaseManagerImpl;
+import com.ming800.core.p.controller.BannerController;
+import com.ming800.core.p.controller.DocumentController;
+import com.ming800.core.p.model.Banner;
 import com.ming800.core.base.service.BaseManager;
 import com.ming800.core.does.model.XQuery;
+import com.ming800.core.p.model.Document;
+import com.ming800.core.p.model.DocumentAttachment;
+import com.ming800.core.p.model.DocumentContent;
+import junit.framework.Assert;
 import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+
 import javax.annotation.Resource;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -36,11 +41,10 @@ import java.util.List;
  * @BeforeClass –> @Before –> @Test –> @After –> @AfterClass
  */
 @SuppressWarnings("unchecked")
-@Transactional
-@TransactionConfiguration(transactionManager ="transactionManager", defaultRollback = true)
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"file:src/main/webapp//WEB-INF/applicationContext-*.xml",
-        "file:src/main/webapp//WEB-INF/spring-servlet.xml"})
+//@Transactional
+//@TransactionConfiguration(transactionManager ="transactionManager", defaultRollback = true)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations={"file:src/main/webapp//WEB-INF/applicationContext-*.xml","file:src/main/webapp//WEB-INF/spring-servlet.xml"})
 @Controller
 public class BannerControllerTest {
     private static Logger logger = Logger.getLogger(BannerController.class);
@@ -53,6 +57,8 @@ public class BannerControllerTest {
      ModelAndView mv ;
      @Resource
      BannerController bannerController;
+    @Resource
+    DocumentController documentController;
      ModelMap  map ;
     @Autowired
     private BaseManager baseManager;
@@ -65,13 +71,17 @@ public class BannerControllerTest {
     public void init() {
           //logger.info("加载spring配置开始 ............");
 	      /* ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml"); */
-         //service = (CatalogGqRepository) ctx.getBean("cataGqRepository");
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext(new String[]{"src/main/webapp//WEB-INF/applicationContext-*.xml",
+                "src/main/webapp//WEB-INF/spring-servlet.xml"});
          //logger.info("加载spring配置结束.............");
         map = new ModelMap();
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
         request.setCharacterEncoding("UTF-8");
         //bannerController = new BannerController();
+        baseManager=(BaseManagerImpl)applicationContext.getBean("baseManagerImpl");
+        bannerController = (BannerController)applicationContext.getBean("bannerController");
+        documentController = (DocumentController)applicationContext.getBean("documentController");
     }
     @Test
     public void testdemo1() {
@@ -102,9 +112,15 @@ public class BannerControllerTest {
     public void testdemo2()throws  Exception{
 
         mv =bannerController.getBannerByModlueId(map, request);
+       /* Assert.assertNotNull(mv);
+        Assert.assertEquals(response.getStatus(), 200);*/
+    }
+    @Test
+    public void testdemo3()throws  Exception{
+
+        documentController.getDocById(map,request);
 
     }
-
     @After
     public  void setUpAfter() throws Exception {
 
