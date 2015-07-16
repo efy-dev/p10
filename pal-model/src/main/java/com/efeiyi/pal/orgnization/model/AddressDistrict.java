2d60634e651493e6d3fd1e804955ffb598171587
jiayankun <jiayankun24@foxmail.com>
2015-07-16 20:46:45 +0800

    成品宝model

---
+++ pal-model/src/main/java/com/efeiyi/pal/orgnization/model/AddressDistrict.java
@@ -0,0 +1,49 @@
+package com.efeiyi.pal.orgnization.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * User: Kyll
+ * Time: 2008-7-3 15:27:28
+ */
+@Entity
+@Table(name = "organization_address_district")
+public class AddressDistrict implements Serializable {
+    private String id;
+    private String name;
+    private AddressCity addressCity;
+
+    @Id
+    @GenericGenerator(name = "id", strategy = "com.ming800.core.p.model.M8idGenerator")
+    @GeneratedValue(generator = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "opened_city_id")
+    public AddressCity getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(AddressCity addressCity) {
+        this.addressCity = addressCity;
+    }
+}
