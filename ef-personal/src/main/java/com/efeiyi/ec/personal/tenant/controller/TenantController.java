a3cceeb914da7ce133a2041ef141993fa3579807
efeiyi007 <z0123456>
2015-07-20 13:13:08 +0800

    提交获取传承人及传承人XQuery配置文件

--- ef-personal/src/main/java/com/efeiyi/ec/personal/tenant/controller/TenantController.java
+++ ef-personal/src/main/java/com/efeiyi/ec/personal/tenant/controller/TenantController.java
@@ -1,16 +1,16 @@
 package com.efeiyi.ec.personal.tenant.controller;
 
-import com.efeiyi.ec.tenant.model.*;
+import com.efeiyi.ec.tenant.model.Tenant;
 import com.ming800.core.base.controller.BaseController;
 import com.ming800.core.base.service.BaseManager;
+import com.ming800.core.does.model.XQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -25,26 +25,14 @@ public class TenantController extends BaseController {
 
     /**
      * 获取某传承人的详情页
-     * @param tenantId
      * @param model
      * @return
      */
     @RequestMapping("/getTenant.do")
-    public ModelAndView getTenant(String tenantId , ModelMap model){
-        LinkedHashMap queryParamMap = new LinkedHashMap();
+    public ModelAndView getTenant(HttpServletRequest request , ModelMap model)throws Exception{
+        String tenantId = request.getParameter("tenantId");
         Tenant tenant = (Tenant) baseManager.getObject(Tenant.class.getName(),tenantId);
-        String queryHql = "from TenantProduct p where p.tenant.id = :tenantId order by p.createDateTime desc";
-        queryParamMap.put("tenantId",tenantId);
-        List productList = baseManager.listObject(queryHql, queryParamMap);
-        List listProduct = new ArrayList();
-        for(int i = 0;i<productList.size();i++){
-            if (i>2){
-                break;
-            }
-            listProduct.add(productList.get(i));
-        }
-        model.addAttribute("entity",tenant);
-        model.addAttribute("productList", listProduct);
+        model.addAttribute("entity", tenant);
         return new ModelAndView("/tenant/tenantView", model);
     }
 
@@ -54,12 +42,10 @@ public class TenantController extends BaseController {
      * @return
      */
     @RequestMapping("/tenantList.do")
-    public ModelAndView listTenant(ModelMap model){
-        String queryHql = "from Tenant";
-        List list = baseManager.listObject(queryHql);
-        if (list != null && list.size() > 0){
-            model.addAttribute("tenantList",list);
-        }
+    public ModelAndView listTenant(HttpServletRequest request , ModelMap model)throws Exception{
+        XQuery xQuery = new XQuery("listTenant_default",request);
+        List list = baseManager.listObject(xQuery);
+        model.addAttribute("list",list);
         return new ModelAndView("/tenant/tenantList",model);
     }
 
