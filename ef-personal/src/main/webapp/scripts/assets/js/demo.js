12c7c4e8614e973a9d4645ae20c0e9558c1573fe
efeiyi007 <z0123456>
2015-07-23 09:24:59 +0800

    修改一些大首页需要的样式文件~~

--- ef-personal/src/main/webapp/scripts/assets/js/demo.js
+++ ef-personal/src/main/webapp/scripts/assets/js/demo.js
@@ -3,7 +3,7 @@ $('.box').each(function(){
 		$(this).mouseover(function(){
 			$(this).children(".BG").css('background','none');
 			$(this).children(".txt").css('background','#fff').css('color','#000')
-			$(this).children("#img-mutual-text1").css('color','#000');
+			$(this).children(".img-mutual-text1").css('color','#000');
 		});
 	});
 
@@ -12,7 +12,7 @@ $('.box').each(function(){
 			$(this).children(".BG").css('background','rgba(60,96,140,0.3)');
 			//$('.BG').css('background','rgba(60,96,140,0.3)');
 			$(this).children(".txt").css('background','url(images/tx12.gif) bottom').css('color','#fff')
-			$(this).children("#img-mutual-text1").css('color','#fff');
+			$(this).children(".img-mutual-text1").css('color','#fff');
 		});
 	})
 }
