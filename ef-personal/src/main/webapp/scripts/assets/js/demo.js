8bd8da6e540e475ead230913a7a7e5ef8acef297
efeiyi007 <z0123456>
2015-07-26 18:12:21 +0800

    排除状态为0的传承人~~

--- ef-personal/src/main/webapp/scripts/assets/js/demo.js
+++ ef-personal/src/main/webapp/scripts/assets/js/demo.js
@@ -11,7 +11,7 @@ $('.box').each(function(){
 		$(this).mouseout(function(){
 			$(this).children(".BG").css('background','rgba(60,96,140,0.3)');
 			//$('.BG').css('background','rgba(60,96,140,0.3)');
-			$(this).children(".txt").css('background','url(images/tx12.gif) bottom').css('color','#fff')
+			$(this).children(".txt").css('background','url(scripts/assets/images/tx12.gif) bottom').css('color','#fff')
 			$(this).children(".img-mutual-text1").css('color','#fff');
 		});
 	})
