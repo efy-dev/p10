08a5872f0ad0428f91b0b3078781322203721b0d
efeiyi007 <z0123456>
2015-07-26 20:44:42 +0800

    工作坊视频处理~~

--- ef-personal/src/main/webapp/scripts/assets/js/demo.js
+++ ef-personal/src/main/webapp/scripts/assets/js/demo.js
@@ -3,7 +3,8 @@ $('.box').each(function(){
 		$(this).mouseover(function(){
 			$(this).children(".BG").css('background','none');
 			$(this).children(".txt").css('background','#fff').css('color','#000')
-			$(this).children(".img-mutual-text1").css('color','#000');
+			/*$(this).children(".img-mutual-text1").css('color','#000');*/
+            $(this).children(".img-mutual-text1-a").css('color','#000');
 		});
 	});
 
@@ -12,7 +13,8 @@ $('.box').each(function(){
 			$(this).children(".BG").css('background','rgba(60,96,140,0.3)');
 			//$('.BG').css('background','rgba(60,96,140,0.3)');
 			$(this).children(".txt").css('background','url(scripts/assets/images/tx12.gif) bottom').css('color','#fff')
-			$(this).children(".img-mutual-text1").css('color','#fff');
+			/*$(this).children(".img-mutual-text1").css('color','#fff');*/
+            $(this).children(".img-mutual-text1-a").css('color','#fff');
 		});
 	})
 }
