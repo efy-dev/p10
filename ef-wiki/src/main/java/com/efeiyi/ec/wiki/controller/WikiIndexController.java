339af9755f251ea13f91a73383b2171ab3c41956
kaysonyang <kayson_yang@163.com>
2015-07-31 13:24:05 +0800

    module for wiki

---
+++ ef-wiki/src/main/java/com/efeiyi/ec/wiki/controller/WikiIndexController.java
@@ -0,0 +1,60 @@
+package com.efeiyi.ec.wiki.controller;
+
+import com.ming800.core.base.controller.BaseController;
+import com.ming800.core.base.service.BaseManager;
+import com.ming800.core.does.model.XQuery;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/7/31.
+ *
+ */
+@Controller
+public class WikiIndexController extends BaseController {
+    private static Logger logger = Logger.getLogger(WikiIndexController.class);
+    @Autowired
+    BaseManager baseManager;
+
+    /**
+     * 非遗百科首页展示
+     * @param
+     *
+     * @return
+     */
+    @RequestMapping("/home.do")
+    public ModelAndView getTenant( HttpServletRequest request  , Model model)throws Exception{
+        logger.info("weiki  for efeiyi index begin...");
+        model.addAttribute("bannerList",getWikiBannerList(request));
+        model.addAttribute("projectList",getFeiyiProjectList(request));
+        logger.info("weiki for efeiyi index end...");
+        return new ModelAndView("/wiki/showIndex");
+
+    }
+
+    /**
+     * getWikiBannerList
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public List getWikiBannerList(HttpServletRequest request)throws Exception{
+        XQuery xQuery = new XQuery("listWikiBanner_default",request);
+        List bannerList = baseManager.listObject(xQuery);
+        return bannerList;
+    }
+
+    public List getFeiyiProjectList(HttpServletRequest request)throws Exception{
+        XQuery xQuery = new XQuery("listWikiProject_default",request);
+        List projectList = baseManager.listObject(xQuery);
+        return  projectList;
+    }
+
+}
---
+++ ef-wiki/src/main/webapp/layouts/main.jsp
@@ -0,0 +1,60 @@
+<!DocType html>
+<%@ page import="com.ming800.core.util.HttpUtil" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+    <title id="page-title">
+        <sitemesh:write property="title"/>
+    </title>
+<script>
+    $.ajax({
+        type: "post",
+        url: '<c:url value="/getTenant.do"/>',
+        cache: false,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            $("#page-title").html(data.fullName);
+        }
+    });
+</script>
+    <%
+        if(HttpUtil.isPhone(request)){
+    %>
+    <%@include file="mobileStyle.jsp"%>
+    <%
+    }else{
+    %>
+    <%@include file="pcStyle.jsp"%>
+    <%
+        }
+    %>
+    <sitemesh:write property='head'/>
+</head>
+<body>
+    <%
+    if(HttpUtil.isPhone(request)){
+    %>
+<%@include file="mobileHeader.jsp"%>
+    <%
+    }else{
+    %>
+<%@include file="pcHeader.jsp"%>
+    <%
+        }
+    %>
+<sitemesh:write property='body'/>
+    <%
+        if(HttpUtil.isPhone(request)){
+    %>
+    <%@include file="mobileFooter.jsp"%>
+    <%
+    }else{
+    %>
+    <%@include file="pcFooter.jsp"%>
+    <%
+        }
+    %>
+</body>
+</html>
