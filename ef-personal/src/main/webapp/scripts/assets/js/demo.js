843405b5951dc4f3c3da7be4a3ee65610893027f
efeiyi007 <z0123456>
2015-07-31 10:06:59 +0800

    聚合页样式更新~~

--- ef-personal/src/main/webapp/scripts/assets/js/demo.js
+++ ef-personal/src/main/webapp/scripts/assets/js/demo.js
@@ -1,20 +1,14 @@
-window.onload=function(){
-$('.box').each(function(){
-		$(this).mouseover(function(){
-			$(this).children(".BG").css('background','none');
-			$(this).children(".txt").css('background','#fff').css('color','#000')
-			/*$(this).children(".img-mutual-text1").css('color','#000');*/
-            $(this).children(".img-mutual-text1-a").css('color','#000');
-		});
-	});
-
-$('.box').each(function(){
-		$(this).mouseout(function(){
-			$(this).children(".BG").css('background','rgba(60,96,140,0.3)');
-			//$('.BG').css('background','rgba(60,96,140,0.3)');
-			$(this).children(".txt").css('background','url(scripts/assets/images/tx12.gif) bottom').css('color','#fff')
-			/*$(this).children(".img-mutual-text1").css('color','#fff');*/
-            $(this).children(".img-mutual-text1-a").css('color','#fff');
-		});
+$(function(){
+	$('.box').hover(function(){
+		$(this).children(".BG").css('background','none');
+		$(this).children(".txt").css('background','#fff').css('color','#000')
+		$(this).children(".img-mutual-text1").css('color','#000').find('a').css('color','#000');
+		$(this).children("a:link,a:visited").css('color','#000');
+	},function(){
+		$(this).children(".BG").css('background','rgba(60,96,140,0.3)');
+		$(this).children(".txt").css({'background':''});
+		$(this).children(".img-mutual-text1").css('color','#fff').find('a').css('color','#fff');
+		$(this).children("a:link,a:visited").css('color','#fff');
 	})
-}
+})
+
