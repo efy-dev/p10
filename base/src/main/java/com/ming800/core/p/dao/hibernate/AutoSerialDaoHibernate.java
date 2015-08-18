1738eabc557542d2864d5cbab69a1756f3d873a8
kaysonyang <kayson_yang@163.com>
2015-08-18 18:49:45 +0800

    增加自动获取序列

--- base/src/main/java/com/ming800/core/p/dao/hibernate/AutoSerialDaoHibernate.java
+++ base/src/main/java/com/ming800/core/p/dao/hibernate/AutoSerialDaoHibernate.java
@@ -3,7 +3,11 @@ package com.ming800.core.p.dao.hibernate;
 import com.ming800.core.base.dao.hibernate.BaseDaoSupport;
 import com.ming800.core.p.dao.AutoSerialDao;
 import com.ming800.core.p.model.AutoSerial;
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,5 +18,13 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class AutoSerialDaoHibernate extends BaseDaoSupport<AutoSerial> implements AutoSerialDao {
-
+    public Long getAutoSerial(String queryStr, LinkedHashMap<String, Object> queryParamMap){
+        Query query = this.getSession().createSQLQuery(queryStr).setString("groupName", queryParamMap.get("groupName").toString());
+        Long serial=null;
+        List list = query.list();
+        if(list.size()>=1){
+            serial=Long.parseLong(list.get(0).toString());
+        }
+        return serial;
+    }
 }
