6470723ee8db91e1d0717da33e763749fa615b69
STUBBORNCOBOY <754767681@qq.com>
2015-08-19 11:10:03 +0800

    后天首页推荐产品修改

---
+++ ef-system/src/main/java/com/efeiyi/ec/system/product/service/ProductModelManager.java
@@ -0,0 +1,8 @@
+package com.efeiyi.ec.system.product.service;
+
+/**
+ * Created by Administrator on 2015/8/17.
+ */
+public interface ProductModelManager {
+    int getMaxRecommendedIndex(String categoryId);
+}
