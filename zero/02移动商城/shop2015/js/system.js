adc25d460ed6de2dff5c7a288f6c5a45e3ae17f4
jiuxuan00 <117898271@qq.com>
2015-08-25 19:27:26 +0800

    移动端---购物车完成

--- zero/02移动商城/shop2015/js/system.js
+++ zero/02移动商城/shop2015/js/system.js
@@ -2,5 +2,14 @@ $(function(){
     //头部无logoA
     $('#menu').click(function(){
         $('.menu-list').slideToggle('fast');
-    })
+    });
+    //购物车
+    (function(){
+        $('.icon-delete').click(function(){
+            $(this).parents('li').slideUp('fast',function(){
+                $(this).remove();
+            });
+        })
+    })();
+
 })
