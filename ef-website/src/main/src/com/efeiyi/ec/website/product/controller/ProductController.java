9f6587e00b3ad4bb1ddbce0aa097f5b01abbc636
jacsonzhou <742565508@qq.com>
2015-08-26 19:15:01 +0800

    商品详情

--- ef-website/src/main/src/com/efeiyi/ec/website/product/controller/ProductController.java
+++ ef-website/src/main/src/com/efeiyi/ec/website/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.efeiyi.ec.organization.model.MyUser;
 import com.efeiyi.ec.product.model.Product;
 import com.efeiyi.ec.product.model.ProductFavorite;
 import com.efeiyi.ec.product.model.ProductModel;
+import com.efeiyi.ec.product.model.ProductPropertyValue;
 import com.efeiyi.ec.project.model.Project;
 import com.efeiyi.ec.purchase.model.Cart;
 import com.efeiyi.ec.purchase.model.CartProduct;
@@ -22,10 +23,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.swing.event.TreeModelListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Administrator on 2015/7/8.
@@ -63,12 +61,7 @@ public class ProductController {
         List<Object> productModelList = baseManager.listPageInfo(xQuery).getList();
         Project project  = (Project)baseManager.getObject(Project.class.getName(),projectId);
         String proName = project.getName();
-        String[] str = new String[16];
-        for(int i = 0;i<proName.length();i++){
-            str[i] = String.valueOf(proName.charAt(i));
-        }
         model.addAttribute("proName",proName);
-        model.addAttribute("strProjectName",str);
         model.addAttribute("project",project);
         model.addAttribute("productModelList",productModelList);
         return "/product/productModelList";
@@ -105,6 +98,7 @@ public class ProductController {
     public String recommendation(@PathVariable String productModelId, HttpServletRequest request, Model model) throws Exception {
         ProductModel productModel = (ProductModel) baseManager.getObject(ProductModel.class.getName(), productModelId);
         List<ProductModel> productModelList  = productModel.getProduct().getProductModelList();
+        productModelList.remove(productModel);
         model.addAttribute("productModelList", productModelList);
         model.addAttribute("productModel", productModel);
         return "/product/recommendationList";
@@ -141,74 +135,22 @@ public class ProductController {
      * @throws Exception
      */
 
-    @RequestMapping({"/category/{productModelId}"})
+  @RequestMapping({"/{productModelId}"})
     public String productDetalis(@PathVariable String productModelId, HttpServletRequest request, Model model){
         ProductModel productModel = (ProductModel) baseManager.getObject(ProductModel.class.getName(), productModelId);
+        List<ProductPropertyValue> productPropertyValueList1 = productModel.getProductPropertyValueList();
+        List<ProductModel> productModelList = productModel.getProduct().getProductModelList();
+        HashMap<String,List<ProductPropertyValue>> map = new HashMap<String,List<ProductPropertyValue>>();
+        //属性和所有属性可能值
+        HashMap<String,List<String>> stringListHashMap = new HashMap<String,List<String>>();
+        for(int i=0;i<productModelList.size();i++){
+            ProductModel productModelTemp = productModelList.get(i);
+            List<ProductPropertyValue> productPropertyValueList = productModelTemp.getProductPropertyValueList();
+        }
+        for(ProductModel pm:productModelList){
+
+        }
         model.addAttribute("productModel", productModel);
         return "/product/productDetails";
-    }
-    /*
-    *
-    * 添加到购物车
-    * */
-    @RequestMapping({"/cart/{productModelId}"})
-    public String addProduct(@PathVariable String productModelId,HttpServletRequest request) throws Exception {
-        // 需要判断用户是否登录
-        Cart cart = null;
-        MyUser currentUser = AuthorizationUtil.getMyUser();
-        if (currentUser.getId() != null) {
-            XQuery xQuery = new XQuery("listCart_default", request);
-            List<Object> list = baseManager.listObject(xQuery);
-            cart = (Cart) list.get(0);
-        } else {
-            Object cartTemp = request.getSession().getAttribute("cart");
-            if (cartTemp != null) {
-                cart = (Cart) cartTemp;
-            } else {
-                cart = new Cart();
-                cart.setCartProductList(new ArrayList<CartProduct>());
-            }
-        }
-
-        boolean ne = false;
-        boolean ab = false;
-
-        XQuery xQuery1 = new XQuery("listCartProduct_default", request);
-        xQuery1.put("cart_id", cart.getId());
-        List<Object> list1 = baseManager.listObject(xQuery1);
-
-        if (list1.size() > 0) {
-            for (Object cartProductTemp : list1) {
-                CartProduct cartProduct = (CartProduct) cartProductTemp;
-                if (productModelId.equals(cartProduct.getProductModel().getId())) {
-                    if (null != request.getParameter("amount") && "" != request.getParameter("amount")) {
-                        cartProduct.setAmount(cartProduct.getAmount() + Integer.parseInt(request.getParameter("amount")));
-                    } else {
-                        cartProduct.setAmount(cartProduct.getAmount() + 1);
-                    }
-                    baseManager.saveOrUpdate(CartProduct.class.getName(), cartProduct);
-
-                    ab = true;
-                    ne = true;
-
-                }
-            }
-        }
-        if (!ne) {
-            ProductModel product = new ProductModel();
-            product.setId(productModelId);
-            CartProduct cartProduct = new CartProduct();
-            cartProduct.setProductModel(product);
-            cartProduct.setCart(cart);
-            cartProduct.setStatus("1");
-            if (null != request.getParameter("amount") && "" != request.getParameter("amount")) {
-                cartProduct.setAmount(Integer.parseInt(request.getParameter("amount")));
-            } else {
-                cartProduct.setAmount(1);
-            }
-            baseManager.saveOrUpdate(CartProduct.class.getName(), cartProduct);
-            ab = true;
-        }
-        return "/purchaseOrder/addProductSuccess";
     }
 }
