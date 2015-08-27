efedb542d3931b7f768724f6970fb98dc4a92f38
lylylyly <360682644@qq.com>
2015-08-27 13:37:50 +0800

    null

--- pal-system/src/main/java/com/efeiyi/pal/system/label/controller/LabelController.java
+++ pal-system/src/main/java/com/efeiyi/pal/system/label/controller/LabelController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * Created by Administrator on 2015/7/28.
@@ -38,7 +40,7 @@ public class LabelController {
         return new ModelAndView(resultPage);
     }
 
-    private String labelListSave(LabelBatch labelBatch, String labelBatchId) throws Exception{
+    private String labelListSave(LabelBatch labelBatch, String labelBatchId) throws Exception {
 
         if (labelBatch == null) {
             throw new Exception("Id为" + labelBatchId + "的标签批次不存在!");
@@ -49,7 +51,37 @@ public class LabelController {
         labelBatch.setStatus("2");
         baseManager.saveOrUpdate(labelBatch.getClass().getName(), labelBatch);
 
-        return "redirect:/basic/xm.do?qm=plistLabel_default&conditions=labelBatch.id:"+labelBatch.getId();
+        return "redirect:/basic/xm.do?qm=plistLabel_default&conditions=labelBatch.id:" + labelBatch.getId();
+    }
+
+    @RequestMapping("/downLabelTxt.do")
+    public void downloadLabelTxt(HttpServletRequest request, HttpServletResponse response) throws Exception{
+
+        String labelBatchId = request.getParameter("labelBatchId");
+        String fileName = labelBatchId + ".txt";
+        File file = new File(fileName);
+//        response.setContentType("application/force-download");
+        response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+        response.setContentLength((int) file.length());
+        BufferedReader bis = null;
+        try {
+            if (!file.exists())
+                throw new NullPointerException("指定文件" + fileName + "不存在");
+            bis = new BufferedReader(new FileReader(new File(fileName)));
+            int i;
+            while ((i = bis.read()) != -1) {
+                response.getWriter().write(i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                bis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
