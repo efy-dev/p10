431624f0d0c060519cbcff592cd07999a6728075
notbadwt <notbad_wangt@126.com>
2015-08-28 15:19:11 +0800

    调整央视文件目录结构

---
+++ ef-website/src/main/webapp/scripts/wap/js/system.js
@@ -0,0 +1,48 @@
+$(function(){
+    //头部无logoA
+    $('#menu').click(function(){
+        $(this).find('.line').toggleClass('active');
+        $('.menu-list').slideToggle('fast');
+    });
+    //购物车
+    (function(){
+        $('.icon-delete').click(function(){
+           $('.alert-delete').fadeIn('fast');
+        })
+
+        $('.edit-dete').click(function(){
+            $('.alert-delete').fadeIn('fast');
+            return false;
+        })
+
+        if($('div').hasClass('payment-total-bar')){
+            $('article').css({'padding-bottom':'50px'})
+        }
+    })();
+    //020102分类导航
+    (function(){
+        $('.box-hd .ul-index-list>li .item').click(function(){
+            $(this).find('.arrow-down').toggleClass('arrow-up');
+            $(this).parents('.ul-index-list').siblings('.ul-nav-list').slideToggle('fast');
+            return false;
+        })
+    })();
+    //020104商品详情
+    (function(){
+        //收藏
+        $('.addfav').click(function(){
+            $(this).addClass('addfav-end').find('p').html('已收藏');
+            return false;
+        });
+        //加入购物车悬浮层
+        if($('div').hasClass('details-total-bar')){
+            $('footer').css({'padding-bottom':'50px'})
+        };
+
+    })();
+
+
+
+
+
+})
