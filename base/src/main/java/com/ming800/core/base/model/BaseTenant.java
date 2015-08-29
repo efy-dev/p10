9267606b1d2f44d08cab9c429833f9e2985a6b12
notbadwt <notbad_wangt@126.com>
2015-08-29 19:31:42 +0800

    添加MY_TENANT系统常量

--- base/src/main/java/com/ming800/core/base/model/BaseTenant.java
+++ base/src/main/java/com/ming800/core/base/model/BaseTenant.java
@@ -8,32 +8,8 @@ import javax.persistence.*;
 /**
  * Created by Administrator on 2015/7/16.
  */
-@Entity
-@Table(name = "tenant")
-@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
-public class BaseTenant {
+public interface BaseTenant {
 
+    String getId();
 
-    private String id;
-    private String name;//名称标识
-
-    @Id
-    @GenericGenerator(name = "id", strategy = "com.ming800.core.p.model.M8idGenerator")
-    @GeneratedValue(generator = "id")
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Column(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
