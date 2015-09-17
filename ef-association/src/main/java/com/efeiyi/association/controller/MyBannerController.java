32948ef61c990617ae32b8a38e74205c17ba9ae7
jiayankun <jiayankun24@foxmail.com>
2015-09-17 17:42:28 +0800

    非遗协会后台更新

---
+++ ef-association/src/main/java/com/efeiyi/association/controller/MyBannerController.java
@@ -0,0 +1,194 @@
+package com.efeiyi.association.controller;
+
+import com.ming800.core.base.service.BaseManager;
+import com.ming800.core.base.service.XdoManager;
+import com.ming800.core.base.service.XdoSupportManager;
+import com.ming800.core.does.model.Do;
+import com.ming800.core.does.model.DoQuery;
+import com.ming800.core.does.model.PageInfo;
+import com.ming800.core.does.model.XQuery;
+import com.ming800.core.does.service.DoManager;
+import com.ming800.core.p.model.Banner;
+import com.ming800.core.p.model.Document;
+import com.ming800.core.p.service.AliOssUploadManager;
+import com.ming800.core.p.service.BannerManager;
+import com.ming800.core.taglib.PageEntity;
+import com.ming800.core.util.ApplicationContextUtil;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by kayson_yang on 2015/7/13.
+ */
+
+@Controller
+@RequestMapping("/myBanner")
+public class MyBannerController {
+    private static Logger logger = Logger.getLogger(MyBannerController.class);
+    @Autowired
+    private BaseManager baseManager;
+
+    @Autowired
+    private BannerManager bannerManager;
+    @Autowired
+    private XdoManager xdoManager;
+
+    @Autowired
+    private XdoSupportManager xdoSupportManager;
+
+    @Autowired
+    private DoManager doManager;
+
+    @Autowired
+    @Qualifier("aliOssUploadManagerImpl")
+    private AliOssUploadManager aliOssUploadManager;
+
+    /**
+     * 通过模块ID获取相关模块的轮播图（状态为可用）的所有对象
+     */
+    @SuppressWarnings("unchecked")
+    @RequestMapping("/ba.do")
+    public List<Banner> getBannerByGroupId(ModelMap modelMap, HttpServletRequest request) throws Exception {
+        String qm = request.getParameter("qm");
+        if (qm.split("_").length < 2) {
+            throw new Exception("qm:" + qm + "的具体查询部分没有定义即'_'的后半部分没有定义");
+        }
+        //先找到配置文件里的entity
+        Do tempDo = doManager.getDoByQueryModel(qm.split("_")[0]);
+        //再从中找到query的信息
+        DoQuery tempDoQuery = tempDo.getDoQueryByName(qm.split("_")[1]);
+//        modelMap.put("tempDo", tempDo);
+//        modelMap.put("doQueryList", tempDo.getDoQueryList());
+//        String resultPage = tempDo.getResult();
+
+        PageEntity pageEntity = new PageEntity();
+        String pageIndex = request.getParameter("pageEntity.index");
+        String pageSize = request.getParameter("pageEntity.size");
+        if (pageIndex != null) {
+            pageEntity.setIndex(Integer.parseInt(pageIndex));
+            pageEntity.setSize(Integer.parseInt(pageSize));
+        }
+
+        modelMap.put("tabTitle", tempDoQuery.getLabel());
+//                resultPage = "/pc/choiceness";
+        PageInfo pageInfo = xdoManager.listPage(tempDo, tempDoQuery, null, pageEntity);
+        modelMap.put("pageInfo", pageInfo);
+        modelMap.put("pageEntity", pageInfo.getPageEntity());
+
+//                返回列表
+//        Map map = request.getParameterMap();
+//        xdoSupportManager.generateTempPageConditions(request.getRequestURI(), map, pageEntity.getIndex() + "", pageEntity.getSize() + "");
+        // xdoSupportManager.generateTempPageConditions(request.getRequestURI(), map, 1 + "", 20 + "");
+
+
+        if (tempDo.getExecute() != null && !tempDo.getExecute().equals("")) {
+            modelMap = xdoSupportManager.execute(tempDo, modelMap, request);
+        }
+        modelMap.put("qm", qm);
+        return pageInfo.getList();
+    }
+
+    /**
+     * 保存
+     *
+     * @param banner
+     * @return
+     */
+    @RequestMapping(value = "/saveBanner.do", method = RequestMethod.POST)
+    public ModelAndView saveBanner(@RequestParam(value = "image") MultipartFile multipartFile, HttpServletRequest request, Banner banner) throws IOException {
+
+        String path = request.getParameter("qm");
+        //新建内容
+        if (banner.getId() == null || "".equals(banner.getId())) {
+            banner.setStatus("1");
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String identify = sdf.format(new Date());
+        String url = "product/" + identify + ".jpg";
+
+        if (!multipartFile.getOriginalFilename().equals("")) {
+            aliOssUploadManager.uploadFile(multipartFile, "315pal", url);
+            banner.setImageUrl(url);
+        }
+        bannerManager.saveBanner(banner);
+        return new ModelAndView("redirect:" + request.getContextPath() + path);
+    }
+
+    /**
+     * 真删
+     *
+     * @param banner
+     * @return
+     */
+    @RequestMapping("/deleteBanner.do")
+    @ResponseBody
+    public String deleteBanner(Banner banner) {
+
+        try {
+
+            bannerManager.deleteBanner(banner);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return banner.getId();
+    }
+
+    /**
+     * 假删
+     *
+     * @param banner
+     * @return
+     */
+    @RequestMapping("/removeBanner.do")
+    @ResponseBody
+    public ModelAndView removeDocument(HttpServletRequest request, Banner banner) throws Exception {
+        String path = request.getContextPath() + request.getParameter("resultPage");
+
+        bannerManager.removeBanner(banner);
+
+        return new ModelAndView("redirect:" + request.getContextPath() + path);
+    }
+
+    /**
+     * 新建页面
+     *
+     * @param banner
+     * @return
+     */
+    @RequestMapping("/newBanner.do")
+    public ModelAndView direct2Jsp(ModelMap model, HttpServletRequest request, Banner banner) throws Exception {
+        String qm = request.getParameter("qm");
+
+        if (qm.split("_").length < 2) {
+            throw new Exception("qm:" + qm + "的具体查询部分没有定义即'_'的后半部分没有定义");
+        }
+        //先找到配置文件里的entity
+        Do tempDo = doManager.getDoByQueryModel(qm.split("_")[0]);
+        //再从中找到query的信息
+//            DoQuery tempDoQuery = tempDo.getDoQueryByName(qm.split("_")[1]);
+        //设置保存后的返回页面
+        model.addAttribute("qm", request.getParameter("resultPage"));
+        if (banner.getId() != null && !"".equals(banner.getId())) {
+            banner = (Banner) baseManager.getObject(banner.getClass().getName(), banner.getId());
+            model.addAttribute("object", banner);
+        }
+        return new ModelAndView(request.getContextPath() + tempDo.getResult());
+    }
+
+
+}
---
+++ ef-association/src/main/webapp/WEB-INF/jsp/myDocument/doc.jsp
@@ -0,0 +1,97 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2015/9/7
+  Time: 13:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt_rt" %>
+<%@ taglib prefix="ming800" uri="http://java.ming800.com/taglib" %>
+<%@include file="/layouts/public.jsp"%>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<div class="am-g">
+    <button onclick="window.location.href='<c:url
+            value="/myDocument/newDocument.do?qm=direct2Jsp_newDocument&document.group=${document.group}&resultPage=/myDocument/doc.do?qm=${requestScope.qm}"/>'"
+            class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span class="am-icon-trash-o"></span>新建
+    </button>
+
+    <table class="am-table am-table-bordered am-table-radius am-table-striped">
+        <tr>
+            <td>操作</td>
+            <td>标题</td>
+            <td>图片</td>
+            <td>发布人</td>
+            <td>创建时间</td>
+        </tr>
+
+        <c:forEach items="${documentList}" var="document">
+            <tr>
+                <td>
+                    <button onclick="window.location.href='<c:url
+                            value="/myDocument/newDocument.do?qm=direct2Jsp_newDocument&id=${document.id}&resultPage=/myDocument/doc.do?qm=${requestScope.qm}"/>'"
+                            class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span
+                            class="am-icon-trash-o"></span> 编辑
+                    </button>
+                    <button onclick="window.location.href='<c:url
+                            value="/myDocument/removeDocument.do?qm=removeContent&id=${document.id}&resultPage=/myDocument/doc.do?qm=${requestScope.qm}"/>'"
+                            class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span
+                            class="am-icon-trash-o"></span> 删除
+                    </button>
+                    <button onclick="saveObject('${document.id}','<c:url
+                            value='/myDocument/saveDocument.do'/>','status',<c:if test="${document.status == 1}">'2'</c:if><c:if test="${document.status != 1}">'1'</c:if>,'&qm=/myDocument/doc.do?qm=plistNewsOrganization_default')"
+                            class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span
+                            class="am-icon-trash-o"></span> <c:if test="${document.status == 1}">取消推荐</c:if><c:if test="${document.status != 1}">推荐</c:if>
+                    </button>
+                    <input id="${document.id}0" value="${document.id}" name="${document.id}" metaProperty="id" type="hidden"/>
+                    <input id="${document.id}1" value="${document.documentContent.id}" name="${document.id}" metaProperty="documentContent.id"
+                           type="hidden"/>
+                    <input id="${document.id}2" value="${document.documentOrder}" name="${document.id}" metaProperty="documentOrder" type="hidden"/>
+                    <input id="${document.id}3" value="${document.keywords}" name="${document.id}" metaProperty="keywords" type="hidden"/>
+                    <input id="${document.id}4" value="${document.sampleContent}" name="${document.id}" metaProperty="sampleContent" type="hidden"/>
+                    <input id="${document.id}5"
+                           value="<fmt:formatDate value="${document.theDatetime}" pattern="yyyy-MM-dd HH:mm"/>"
+                           name="${document.id}" metaProperty="theDatetime" type="hidden"/>
+                    <input id="${document.id}6"
+                           value="<fmt:formatDate value="${document.publishDate}" pattern="yyyy-MM-dd HH:mm"/>"
+                           name="${document.id}" metaProperty="publishDate" type="hidden"/>
+                    <input id="${document.id}7" value="${document.status}" name="${document.id}" type="hidden" metaProperty="status"/>
+                    <input id="${document.id}8" value="${document.group}" name="${document.id}" metaProperty="group" type="hidden"/>
+                    <input id="${document.id}9" value="${document.documentContent.content}" metaProperty="documentContent.content" name="${document.id}"
+                           type="hidden"/>
+                </td>
+                <td>
+                    <div name="title">${document.title}</div>
+                    <input type="hidden" id="${document.id}10" name="${document.id}" value="${document.title}" metaProperty="title">
+                </td>
+                <td>
+                    <c:forEach items="${documentAttachmentList}" var="attachment">
+                        <img  id="${document.id}11" name="${document.id}" src="<%=imgBasePath%>${attachment.path}<%=imgListCss%>">
+                    </c:forEach>
+                </td>
+                <td>
+                    <div name="name">${document.name}</div>
+                    <input type="hidden" id="${document.id}12" name="${document.id}" value="${document.name}" metaProperty="name">
+                </td>
+                <td>
+                    <div id="${document.id}13"><fmt:formatDate value="${document.publishDate}"
+                                                               pattern="yyyy-MM-dd HH:mm"/></div>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+</div>
+<div style="clear: both">
+    <c:url value="/myDocument/doc.do" var="url"/>
+    <ming800:pcPageList bean="${requestScope.pageInfo.pageEntity}" url="${url}">
+        <ming800:pcPageParam name="qm" value="${requestScope.qm}"/>
+        <ming800:pcPageParam name="conditions" value="${requestScope.conditions}"/>
+    </ming800:pcPageList>
+</div>
+</body>
+</html>
