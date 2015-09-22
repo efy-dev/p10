fb39b1ef75e78a59f73a86694b6a27003e648f3e
lylylyly <360682644@qq.com>
2015-09-22 20:41:04 +0800

    null

---
+++ ef-association-system/src/main/java/com/efeiyi/association/core/DatabaseContextHolder.java
@@ -0,0 +1,20 @@
+package com.efeiyi.association.core;
+
+/**
+ * Created by Administrator on 2015/9/22.
+ */
+public class DatabaseContextHolder {
+    private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
+
+    public static void setCustomerType(String customerType) {
+        contextHolder.set(customerType);
+    }
+
+    public static String getCustomerType() {
+        return contextHolder.get();
+    }
+
+    public static void clearCustomerType() {
+        contextHolder.remove();
+    }
+}
