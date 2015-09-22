a562cb95fd700e837c974c3f3eaf9b68a01379af
zzchong <zz_chong@163.com>
2015-09-22 11:40:14 +0800

    商家用户 关联项目 关联大师

---
+++ ef-tenant/src/main/java/com/efeiyi/ec/system/organization/service/TenantManager.java
@@ -0,0 +1,16 @@
+package com.efeiyi.ec.system.organization.service;
+
+/**
+ * Created by Administrator on 2015/8/17.
+ */
+public interface TenantManager {
+    /**
+     * 关联商家
+     * @param tenantId
+     * @param projectId
+     * @param tenantProjectId
+     * @param status
+     * @return
+     */
+   String linkProject(String tenantId, String projectId, String tenantProjectId, String status);
+}
