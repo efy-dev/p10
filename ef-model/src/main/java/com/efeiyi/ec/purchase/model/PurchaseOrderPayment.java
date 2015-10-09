5220103977a3826bf5e20904b2e6a26563cad6fd
STUBBORNCOBOY <754767681@qq.com>
2015-10-09 18:03:22 +0800

    支付完成后修改支付记录状态

--- ef-model/src/main/java/com/efeiyi/ec/purchase/model/PurchaseOrderPayment.java
+++ ef-model/src/main/java/com/efeiyi/ec/purchase/model/PurchaseOrderPayment.java
@@ -23,7 +23,7 @@ public class PurchaseOrderPayment {
     private List<PurchaseOrderPaymentDetails> purchaseOrderPaymentDetailsList;
     private User user;
     private String serial;//支付记录编号
-    private String status;//1支付成功  2支付失败 3 待支付
+    private String status;//1待支付  2已支付 3 支付失败~
 
     @Id
     @GenericGenerator(name = "id", strategy = "com.ming800.core.p.model.M8idGenerator")
