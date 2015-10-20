f9a629a8b96e33addcdc0be64b326e65e7b5629a
zzchong <zz_chong@163.com>
2015-10-20 17:15:12 +0800

    商家用户修改

--- ef-system/src/main/java/com/efeiyi/ec/system/professional/service/impl/ProfessionalManagerImpl.java
+++ ef-system/src/main/java/com/efeiyi/ec/system/professional/service/impl/ProfessionalManagerImpl.java
@@ -36,23 +36,32 @@ public class ProfessionalManagerImpl implements ProfessionalManager{
     public String saveProfessional(Professional professional) throws Exception{
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         String date = sdf.format(new Date());
-        if("".equals(professional.getPassword())){
-            Professional tempProfessional =(Professional)xdoDao.getObject(Professional.class.getName(), professional.getId());
-            professional.setPassword(tempProfessional.getPassword());
-        }
-        professional.setPassword(StringUtil.encodePassword(professional.getPassword(), "SHA"));
-        professional.setStatus("1");
-        professional.setEnabled(true);
-        professional.setAccountExpired(false);
-        professional.setAccountLocked(false);
-        professional.setCredentialsExpired(false);
+        Professional tempProfessional = null;
         if("".equals(professional.getId())){
-            professional.setId(null);
+            tempProfessional = new Professional();
+        }else {
+            tempProfessional =(Professional)xdoDao.getObject(Professional.class.getName(), professional.getId());
         }
+        if(!"".equals(professional.getPassword())){
+
+            professional.setPassword(StringUtil.encodePassword(professional.getPassword(), "SHA"));
+        }
+        tempProfessional.setBigTenant(professional.getBigTenant());
+        tempProfessional.setName(professional.getName());
+        tempProfessional.setUsername(professional.getUsername());
+        tempProfessional.setPhone(professional.getPhone());
+        tempProfessional.setSex(professional.getSex());
+        tempProfessional.setEmail(professional.getEmail());
+        tempProfessional.setStatus("1");
+        tempProfessional.setEnabled(true);
+        tempProfessional.setAccountExpired(false);
+        tempProfessional.setAccountLocked(false);
+        tempProfessional.setCredentialsExpired(false);
+
         if(professional.getCreateDatetime()==null){
-            professional.setCreateDatetime(sdf.parse(date));
+            tempProfessional.setCreateDatetime(sdf.parse(date));
         }
-        xdoDao.saveOrUpdateObject(professional);
-        return professional.getId();
+        xdoDao.saveOrUpdateObject(tempProfessional);
+        return tempProfessional.getId();
     }
 }
