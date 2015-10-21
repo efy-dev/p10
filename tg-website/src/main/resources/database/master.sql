ad2550e0b92ba2197eca8fd80224b60a723771fa
jacsonzhou <742565508@qq.com>
2015-10-21 10:11:20 +0800

    groupproduct提交

--- tg-website/src/main/resources/database/master.sql
+++ tg-website/src/main/resources/database/master.sql
@@ -8,3 +8,15 @@ PRIMARY KEY (`id`)
 )
 ;
 
+CREATE TABLE `group_product` (
+`id`  char(16) NOT NULL ,
+`member_amount`  int(8) NULL ,
+`group_price`  DECIMAL(8) NULL ,
+`create_datetime`  DATETIME NULL ,
+`bonus`  DECIMAL(10) NULL ,
+`group_purchase_time `  DATETIME NULL,
+`product_id `  char(16) NULL,
+`status `  char(16) NOT NULL,
+PRIMARY KEY (`id`)
+)
+;
