8fcefdc64f9f7645532e43182cf77e446952280b
notbadwt <notbad_wangt@126.com>
2015-10-22 18:01:30 +0800

    完善订单子系统的功能

--- ef-website/src/main/java/com/efeiyi/ec/website/order/controller/PurchaseOrderPaymentController.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/order/controller/PurchaseOrderPaymentController.java
@@ -182,106 +182,7 @@ public class PurchaseOrderPaymentController {
     }
 
 
-    @RequestMapping({"/confirm/{orderId}"})
-    public String orderConfirm(@PathVariable String orderId, HttpServletRequest request) throws Exception {
-        String payment = request.getParameter("payment");
-        String isWeiXin = request.getParameter("isWeiXin");//移动网站页面用的
-        String addressId = request.getParameter("address");
-        String message = request.getParameter("message");
-        //买家留言
-        HashMap<String, String> messageMap = new HashMap<>();
-        for (String messageTemp : message.split(";")) {
-            if (messageTemp != null && !messageTemp.equals("")) {
-                if (messageTemp.split(":").length >= 2)
-                    messageMap.put(messageTemp.split(":")[0], messageTemp.split(":")[1]);
-            }
-        }
-        ConsumerAddress consumerAddress = (ConsumerAddress) baseManager.getObject(ConsumerAddress.class.getName(), addressId);
-        String purchaseOrderAddress = (consumerAddress.getProvince() != null ? consumerAddress.getProvince().getName() : "") + " " + (consumerAddress.getCity() != null ? consumerAddress.getCity().getName() : "") + " " + (consumerAddress.getDetails() != null ? consumerAddress.getDetails() : "");
-        PurchaseOrder purchaseOrder = (PurchaseOrder) baseManager.getObject(PurchaseOrder.class.getName(), orderId);
-        purchaseOrder.setStatus("1");
-        purchaseOrder.setPayWay(payment);
-//        purchaseOrder.setConsumerAddress(consumerAddress);
-        purchaseOrder.setPurchaseOrderAddress(purchaseOrderAddress);
-        purchaseOrder.setReceiverName(consumerAddress.getConsignee() != null ? consumerAddress.getConsignee() : "");
-        purchaseOrder.setReceiverPhone(consumerAddress.getPhone() != null ? consumerAddress.getPhone() : "");
-        List<PurchaseOrder> subPurchaseOrderList = purchaseOrder.getSubPurchaseOrder();
-        if (subPurchaseOrderList != null && subPurchaseOrderList.size() > 1) {
-            for (PurchaseOrder purchaseOrderTemp : subPurchaseOrderList) {
-                purchaseOrderTemp.setStatus("1");
-                purchaseOrderTemp.setPurchaseOrderAddress(purchaseOrderAddress);
-                purchaseOrderTemp.setReceiverName(consumerAddress.getConsignee() != null ? consumerAddress.getConsignee() : "");
-                purchaseOrderTemp.setReceiverPhone(consumerAddress.getPhone() != null ? consumerAddress.getPhone() : "");
-//                purchaseOrderTemp.setConsumerAddress(consumerAddress);
-                purchaseOrderTemp.setMessage(messageMap.get(purchaseOrderTemp.getTenant().getId() + "Message"));
-                baseManager.saveOrUpdate(PurchaseOrder.class.getName(), purchaseOrderTemp);
-            }
-        } else {
-            purchaseOrder.setMessage(messageMap.get(purchaseOrder.getTenant().getId() + "Message"));
-        }
-        baseManager.saveOrUpdate(PurchaseOrder.class.getName(), purchaseOrder);
-        //生成支付记录以及支付详情
-        PurchaseOrderPayment purchaseOrderPayment = new PurchaseOrderPayment();
-        purchaseOrderPayment.setStatus("1");
-        purchaseOrderPayment.setCreateDateTime(new Date());
-        purchaseOrderPayment.setPaymentAmount(purchaseOrder.getTotal());
-        purchaseOrderPayment.setPurchaseOrder(purchaseOrder);
-        purchaseOrderPayment.setPayWay("1");
-        try {
-            purchaseOrderPayment.setSerial(autoSerialManager.nextSerial("payment"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String userid = AuthorizationUtil.getMyUser().getId();
-        User user = (User) baseManager.getObject(User.class.getName(), userid);
-        purchaseOrderPayment.setUser(user);
-        baseManager.saveOrUpdate(PurchaseOrderPayment.class.getName(), purchaseOrderPayment);
-        //支付详情
-        if (purchaseOrder.getCoupon() != null) {
-            Coupon coupon = purchaseOrder.getCoupon();
-            coupon.setStatus("2");
-            baseManager.saveOrUpdate(Coupon.class.getName(), coupon);
-            PurchaseOrderPaymentDetails purchaseOrderPaymentDetails = new PurchaseOrderPaymentDetails();
-            purchaseOrderPaymentDetails.setCoupon(coupon);
-            purchaseOrderPaymentDetails.setMoney(new BigDecimal(coupon.getCouponBatch().getPrice()));
-            purchaseOrderPaymentDetails.setPayWay("4");
-            purchaseOrderPaymentDetails.setPurchaseOrderPayment(purchaseOrderPayment);
-            baseManager.saveOrUpdate(PurchaseOrderPaymentDetails.class.getName(), purchaseOrderPaymentDetails);
-        }
-        PurchaseOrderPaymentDetails purchaseOrderPaymentDetails = new PurchaseOrderPaymentDetails();
-        if (purchaseOrder.getCoupon() != null) {
-            purchaseOrderPaymentDetails.setMoney(purchaseOrder.getTotal().subtract(new BigDecimal(purchaseOrder.getCoupon().getCouponBatch().getPrice())));
-        } else {
-            purchaseOrderPaymentDetails.setMoney(purchaseOrder.getTotal());
-        }
-        purchaseOrderPaymentDetails.setPayWay(purchaseOrder.getPayWay());
-        purchaseOrderPaymentDetails.setPurchaseOrderPayment(purchaseOrderPayment);
-        baseManager.saveOrUpdate(PurchaseOrderPaymentDetails.class.getName(), purchaseOrderPaymentDetails);
 
-        // 清除购物车
-        XQuery xQuery = new XQuery("listCart_default", request);
-        List<Object> list = baseManager.listObject(xQuery);
-        Cart realCart = (Cart) list.get(0);
-        realCart.setTotalPrice(new BigDecimal(0));
-        baseManager.saveOrUpdate(Cart.class.getName(), realCart);
-        for (CartProduct cartProductTemp : realCart.getCartProductList()) {
-            if (cartProductTemp.getIsChoose().equals("1")) {
-                baseManager.remove(CartProduct.class.getName(), cartProductTemp.getId());
-            }
-        }
-
-        String resultPage = "";
-        if (payment.equals("1")) {//支付宝
-            resultPage = "redirect:/order/pay/alipay/" + purchaseOrderPaymentDetails.getId();
-        } else if (payment.equals("3")) { //微信
-            if (isWeiXin != null) {
-                resultPage = "redirect:/order/pay/weixin/" + purchaseOrderPaymentDetails.getId();
-            } else {
-                resultPage = "redirect:/order/pay/weixin/native/" + purchaseOrderPaymentDetails.getId();
-            }
-        }
-        return resultPage;
-    }
 
 
     @RequestMapping({"/pay/{orderId}"})
@@ -291,43 +192,7 @@ public class PurchaseOrderPaymentController {
         String isWeiXin = request.getParameter("isWeiXin");//移动网站页面用的
         String payment = purchaseOrder.getPayWay();
 
-        //生成支付记录以及支付详情
-        PurchaseOrderPayment purchaseOrderPayment = new PurchaseOrderPayment();
-        purchaseOrderPayment.setStatus("1");
-        purchaseOrderPayment.setCreateDateTime(new Date());
-        purchaseOrderPayment.setPaymentAmount(purchaseOrder.getTotal());
-        purchaseOrderPayment.setPurchaseOrder(purchaseOrder);
-        purchaseOrderPayment.setPayWay("1");
-        try {
-            purchaseOrderPayment.setSerial(autoSerialManager.nextSerial("payment"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String userid = AuthorizationUtil.getMyUser().getId();
-        User user = (User) baseManager.getObject(User.class.getName(), userid);
-        purchaseOrderPayment.setUser(user);
-        baseManager.saveOrUpdate(PurchaseOrderPayment.class.getName(), purchaseOrderPayment);
-        //支付详情
-        if (purchaseOrder.getCoupon() != null) {
-            Coupon coupon = purchaseOrder.getCoupon();
-            coupon.setStatus("2");
-            baseManager.saveOrUpdate(Coupon.class.getName(), coupon);
-            PurchaseOrderPaymentDetails purchaseOrderPaymentDetails = new PurchaseOrderPaymentDetails();
-            purchaseOrderPaymentDetails.setCoupon(coupon);
-            purchaseOrderPaymentDetails.setMoney(new BigDecimal(coupon.getCouponBatch().getPrice()));
-            purchaseOrderPaymentDetails.setPayWay("4");
-            purchaseOrderPaymentDetails.setPurchaseOrderPayment(purchaseOrderPayment);
-            baseManager.saveOrUpdate(PurchaseOrderPaymentDetails.class.getName(), purchaseOrderPaymentDetails);
-        }
-        PurchaseOrderPaymentDetails purchaseOrderPaymentDetails = new PurchaseOrderPaymentDetails();
-        if (purchaseOrder.getCoupon() != null) {
-            purchaseOrderPaymentDetails.setMoney(purchaseOrder.getTotal().subtract(new BigDecimal(purchaseOrder.getCoupon().getCouponBatch().getPrice())));
-        } else {
-            purchaseOrderPaymentDetails.setMoney(purchaseOrder.getTotal());
-        }
-        purchaseOrderPaymentDetails.setPayWay(purchaseOrder.getPayWay());
-        purchaseOrderPaymentDetails.setPurchaseOrderPayment(purchaseOrderPayment);
-        baseManager.saveOrUpdate(PurchaseOrderPaymentDetails.class.getName(), purchaseOrderPaymentDetails);
+        PurchaseOrderPaymentDetails purchaseOrderPaymentDetails  = paymentManager.initPurchaseOrderPayment(purchaseOrder);
 
         if (payment.equals("1")) {//支付宝
             return "redirect:/order/pay/alipay/" + purchaseOrderPaymentDetails.getId();
--- ef-website/src/main/java/com/efeiyi/ec/website/order/service/PurchaseOrderManager.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/order/service/PurchaseOrderManager.java
@@ -1,7 +1,11 @@
 package com.efeiyi.ec.website.order.service;
 
+import com.efeiyi.ec.organization.model.ConsumerAddress;
 import com.efeiyi.ec.purchase.model.Cart;
 import com.efeiyi.ec.purchase.model.PurchaseOrder;
+import org.springframework.ui.Model;
+
+import java.util.HashMap;
 
 /**
  * Created by Administrator on 2015/10/21 0021.
@@ -14,9 +18,9 @@ public interface PurchaseOrderManager {
         2.生成子订单
         3.
      */
-    PurchaseOrder saveOrUpdatePurchaseOrder(Cart cart);
+    PurchaseOrder saveOrUpdatePurchaseOrder(Cart cart,Model model) throws Exception;
 
-    PurchaseOrder confirmPurchaseOrder(PurchaseOrder purchaseOrder);
+    PurchaseOrder confirmPurchaseOrder(PurchaseOrder purchaseOrder ,ConsumerAddress consumerAddress,HashMap<String, String> messageMap,String payWay);
 
 
 
