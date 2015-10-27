905e158ae2ba6f4893376d5fcc596d34ca45bcd7
yhq21 <947219155@qq.com>
2015-10-27 17:29:42 +0800

    电商添加富文本页面显示

---
+++ ef-website/src/main/java/com/efeiyi/ec/website/tenant/controller/RichTextController.java
@@ -0,0 +1,32 @@
+package com.efeiyi.ec.website.tenant.controller;
+
+import com.ming800.core.base.service.BaseManager;
+import com.ming800.core.does.model.XQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/10/26.
+ */
+@Controller
+@RequestMapping("/document")
+public class RichTextController {
+    @Autowired
+    private BaseManager baseManager;
+
+    @RequestMapping("/richTextView")
+    public String getRichText(HttpServletRequest request,Model model) throws Exception {
+        XQuery xQuery=new XQuery("listDocument_default",request);
+        xQuery.put("group",request.getParameter("group"));
+        List<Object> list=baseManager.listObject(xQuery);
+        model.addAttribute("document",list);
+
+        return "/tenant/documentView";
+    }
+
+}
