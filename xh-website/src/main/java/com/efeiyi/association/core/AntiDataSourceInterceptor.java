0436a7cfb49ad4f9c216d14dfd67c756d9586f74
lylylyly <360682644@qq.com>
2015-10-30 18:10:52 +0800

    xh前台数据源切换拦截加入同步锁防止同一页面同时发起多个jsp:include线程共享同一个HttpServletRequest时导致的并发错误

--- xh-website/src/main/java/com/efeiyi/association/core/AntiDataSourceInterceptor.java
+++ xh-website/src/main/java/com/efeiyi/association/core/AntiDataSourceInterceptor.java
@@ -1,36 +1,14 @@
 package com.efeiyi.association.core;
 
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by Administrator on 2015/7/20.
- * 多数据源切换拦截器
+ * Created by Administrator on 2015/10/30.
  */
-public class DataSourceInterceptor extends HandlerInterceptorAdapter {
-
-
-
-
-    /**
-     * 拦截后处理，当前策略为恢复默认数据源
-     *
-     * @param request
-     * @param response
-     * @param o
-     * @param mav
-     * @throws Exception
-     */
-    public void postHandle(HttpServletRequest request,
-                           HttpServletResponse response, Object o, ModelAndView mav)
-            throws Exception {
-
-            DatabaseContextHolder.setDataSource(null);
-
-    }
+public class AntiDataSourceInterceptor extends HandlerInterceptorAdapter {
 
     /**
      * 拦截前处理，当前策略为直接选择第二数据源
@@ -45,8 +23,6 @@ public class DataSourceInterceptor extends HandlerInterceptorAdapter {
             throws Exception {
 
         DatabaseContextHolder.getDataSourceLock(request).lock();
-
-        DatabaseContextHolder.setDataSource("dataSource");
 
         return true;
     }
