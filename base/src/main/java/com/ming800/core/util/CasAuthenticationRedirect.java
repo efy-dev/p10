d7493e543b88f4293d73bfdfeba5f98a97f99e7c
notbadwt <notbad_wangt@126.com>
2015-12-03 10:27:27 +0800

    更新订单流程

--- base/src/main/java/com/ming800/core/util/CasAuthenticationRedirect.java
+++ base/src/main/java/com/ming800/core/util/CasAuthenticationRedirect.java
@@ -1,16 +1,17 @@
-package com.ming800.core.util;
+package authentication;
 
+import com.ming800.core.util.HttpUtil;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
 
 /**
  * Created by Administrator on 2015/11/4.
  */
-public class CasAuthenticationRedirect extends org.springframework.security.cas.web.CasAuthenticationEntryPoint{
-    private String serviceUrlBak=null;
-
+public class CasAuthenticationRedirect extends org.springframework.security.cas.web.CasAuthenticationEntryPoint {
+    private String serviceUrlBak = null;
 
 
     @Override
@@ -18,29 +19,27 @@ public class CasAuthenticationRedirect extends org.springframework.security.cas.
     protected String createServiceUrl(final HttpServletRequest request, final HttpServletResponse response) {
 
 
-        if(serviceUrlBak==null)
+        if (serviceUrlBak == null)
 
 
+            serviceUrlBak = getServiceProperties().getService();
 
-            serviceUrlBak=getServiceProperties().getService();
 
-
-        if(serviceUrlBak!=null){
-            String ctx=request.getContextPath();
-            String queryString=request.getQueryString();
-            String requestURI=request.getRequestURI();
-            requestURI=requestURI.substring(requestURI.indexOf(ctx)+ctx.length(),requestURI.length());
-            String serviceUrl="";
-            if(!requestURI.equals("/") && requestURI.length()>0){
+        if (serviceUrlBak != null) {
+            String ctx = request.getContextPath();
+            String queryString = request.getQueryString();
+            String requestURI = request.getRequestURI();
+            requestURI = requestURI.substring(requestURI.indexOf(ctx) + ctx.length(), requestURI.length());
+            String serviceUrl = "";
+            if (!requestURI.equals("/") && requestURI.length() > 0) {
                 //serviceUrl="?"+ new SimpleUrlAuthenticationSuccessHandler().getTargetUrlParameter();
-                serviceUrl="?callUrl";
-                serviceUrl+="="+requestURI;
-                if(StringUtils.isNotBlank(queryString)){
-                    serviceUrl+="?"+queryString;
+                serviceUrl = "?callUrl";
+                serviceUrl += "=" +request.getServerName()+":"+request.getServerPort()+ requestURI;
+                if (StringUtils.isNotBlank(queryString)) {
+                    serviceUrl += "?" + queryString;
                 }
-
             }
-            getServiceProperties().setService(serviceUrlBak+serviceUrl);
+            getServiceProperties().setService(serviceUrlBak + serviceUrl);
         }
 
 
