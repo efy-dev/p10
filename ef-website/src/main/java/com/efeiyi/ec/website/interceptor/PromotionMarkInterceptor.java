3d4659f576b3bd10b5e8087bbb54a8f5338c6a52
lylylyly <360682644@qq.com>
2015-12-07 16:25:35 +0800

    返利营销拦截试一下部署

---
+++ ef-website/src/main/java/com/efeiyi/ec/website/interceptor/PromotionMarkInterceptor.java
@@ -0,0 +1,55 @@
+package com.efeiyi.ec.website.interceptor;
+
+import com.efeiyi.ec.organization.model.User;
+import com.efeiyi.ec.website.organization.util.AuthorizationUtil;
+import com.efeiyi.jh.advertisement.model.PromotionPlan;
+import com.efeiyi.jh.advertisement.model.PromotionUserRecord;
+import com.ming800.core.base.service.BaseManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashMap;
+
+/**
+ * Created by Administrator on 2015/12/3.
+ */
+public class PromotionMarkInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    private BaseManager baseManager;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String promotionChannel = request.getParameter("pch");
+        if (promotionChannel != null) {
+            HttpSession session = request.getSession();
+            String previousPChannel = (String) session.getAttribute("pch");
+            if (!promotionChannel.equals(previousPChannel)) {
+                if (AuthorizationUtil.isAuthenticated()) {
+                    User user = AuthorizationUtil.getUser();
+                    LinkedHashMap<String, Object> queryParamMap = new LinkedHashMap<>();
+                    queryParamMap.put("user", user);
+                    PromotionUserRecord promotionUserRecord = (PromotionUserRecord) baseManager.getUniqueObjectByConditions("from PromotionUserRecord x where x.user=:user", queryParamMap);
+                    if (promotionUserRecord == null) {
+                        promotionUserRecord = new PromotionUserRecord();
+                    }
+                    promotionUserRecord.setUser(user);
+                    PromotionPlan promotionPlan = (PromotionPlan) baseManager.getObject(PromotionPlan.class.getName(), promotionChannel);
+                    promotionUserRecord.setPromotionPlan(promotionPlan);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.DATE,promotionPlan.getRdDays());
+                    promotionUserRecord.setRdEndDate(calendar.getTime());
+                    baseManager.saveOrUpdate(PromotionUserRecord.class.getName(), promotionUserRecord);
+                }
+                //只以最后一次点击的返利链接为准
+                session.setAttribute("pch", promotionChannel);
+            }
+        }
+        return super.preHandle(request, response, handler);
+    }
+}
