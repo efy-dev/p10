08c91b2d7f599fe4e8e35215e24ed8f43d7f894b
yhq21 <947219155@qq.com>
2015-12-11 17:29:24 +0800

    个人中心头部更改

--- ef-consumer/src/main/webapp/WEB-INF/jsp/pc/common/productCategory.jsp
+++ ef-consumer/src/main/webapp/WEB-INF/jsp/pc/common/productCategory.jsp
@@ -8,17 +8,65 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<span class="bgbor"></span>
+<div class="allsort" style="display: none;">
+    <c:forEach items="${categoryList}" var="category" begin="0" end="8">
+        <div class="item">
+            <c:if test="${category.id==projectCategory.id}">
+            <h3 class="active">
 
-<div class="nav-links" style="display:none;">
-    <c:forEach items="${categoryList}" var="category">
-        <ul>
-            <li class="title">${category.name}</li>
-            <c:forEach items="${projectMap.get(category.id)}" var="project">
-                <li><a href="<%=PConst.HOSTNAME%>/product/list/${project.id}" target="_blank" title="">${project.name}</a></li>
-            </c:forEach>
+                </c:if>
+                <c:if test="${category.id!=projectCategory.id}">
+                <h3>
 
-        </ul>
+                    </c:if><i class="icon-new icon-left"></i><a href="<c:url value="/project/list/${category.id}"/>" title="">${category.name}</a><i class="icon-new icon-link"></i><i class="icon-new icon-right"></i></h3>
+                    <%--<h3 class="active"><i class="icon-new icon-left"></i><a href="<c:url value="/project/list/${category.id}"/>" title="">${category.name}</a><i class="icon-new icon-link"></i><i class="icon-new icon-right"></i></h3>--%>
+                <div class="i-mc" style="display: none;">
+                    <div class="links">
+                        <c:forEach items="${projectMap.get(category.id)}" var="project">
+                            <a href="<%=PConst.HOSTNAME%>/product/list/${project.id}" title="">${project.name}</a>
+                        </c:forEach>
+                            <%--<a class="addeven" href="<c:url value="/product/list/${project.id}"/>" title="查看更多">查看更多</a>--%>
+                    </div>
+                    <!-- //End--links-->
+                        <%--<div class="reco">--%>
+                        <%--<c:forEach items="${projectMap.get(category.id)}" var="project">--%>
+                        <%--<c:if test="${not empty recommendedTenantList}">--%>
+                        <%--<c:forEach items="${recommendedTenantList}" var="tenant">--%>
+                        <%--<c:if test="${project.id==tenantMap.get(tenant.id)}">--%>
+                        <%--<a href="<c:url value="/tenant/${tenant.id}"/>" title=""> <img class="imgfilter"--%>
+                        <%--src="http://pro.efeiyi.com/${tenant.logoUrl}@!project-tenant-pc"--%>
+                        <%--alt=""></a>--%>
+                        <%--</c:if>--%>
+                        <%--</c:forEach>--%>
+                        <%--</c:if>--%>
+                        <%--</c:forEach>--%>
+                        <%--</div>--%>
+                        <%--</c:forEach>--%>
+                </div>
+        </div>
     </c:forEach>
-
 </div>
+<!--//End--nav-new-->
+<style>
+    .nav-new .cate .allsort {
+        width: 180px;
+        height: 380px;
+        padding: 30px 0;
+        position: absolute;
+        top: 30px;
+        left: 0;
+        background: #fff;
+        box-sizing: content-box;
+    }
+
+
+    .nav-new .cate .allsort .item .i-mc .links {
+        width: 240px;
+        height: 384px;
+        padding: 28px 41px;
+        float: left;
+        border-left: 1px solid #e5e5e5;
+        border-right: 1px solid #e5e5e5;
+        box-sizing: content-box;
+    }
+</style>
--- ef-consumer/src/main/webapp/scripts/js/system.js
+++ ef-consumer/src/main/webapp/scripts/js/system.js
@@ -5,11 +5,24 @@ $(function(){
             $(this).find('.top-wechat').stop(true).slideDown('fast');
         },function(){
             $(this).find('.top-wechat').stop(true).slideUp('fast');
+        });
+        //
+        $('.btn-top-nav').hover(function(){
+            $(this).find('.ul-details').stop(true).slideDown('fast');
+        },function(){
+            $(this).find('.ul-details').stop(true).slideUp('fast');
+        });
+        //footer二维码
+        $('.footer .service .wechat').hover(function(){
+            $(this).find('.icon').fadeIn('100');
+        },function(){
+            $(this).find('.icon').fadeOut();
         })
     })();
     //
     (function(){
         $('.header .nav li').hover(function(){
+            $('.header,.footer').css('z-index','');
             $(this).find('.bgbor').show();
             $(this).addClass('hover').find('.nav-links').stop().slideDown('fast');
         },function(){
@@ -19,7 +32,11 @@ $(function(){
     })();
     //
     (function(){
-        var $liImg=$('.focus .slider-main li');
+        var $focus=$('.focus');
+        var $btnPrev=$focus.find('.btn-prev');
+        var $btnNext=$focus.find('.btn-next');
+        var $sliderMain=$('.focus .slider-main');
+        var $liImg=$sliderMain.find('li');
         var $liNav=$('.focus .slider-nav li');
         var index=0;
         var timer=null;
@@ -33,6 +50,40 @@ $(function(){
         $liNav.mouseup(function(){
             timer=setInterval(autoFocus,iSpeed);
         })
+        $sliderMain.hover(function(){
+            clearInterval(timer);
+        },function(){
+            timer=setInterval(autoFocus,iSpeed)
+        })
+
+        $focus.hover(function(){
+            $btnNext.animate({'right':'0'},100);
+            $btnPrev.animate({'left':'0'},100);
+        },function(){
+            $btnNext.animate({'right':'-45px'},100);
+            $btnPrev.animate({'left':'-45px'},100);
+        })
+        //下一页
+        $btnNext.mousedown(function(){
+            clearInterval(timer);
+            autoFocus();
+        })
+        $btnNext.mouseup(function(){
+            timer=setInterval(autoFocus,iSpeed);
+        })
+        //上一页
+        $btnPrev.mousedown(function(){
+            clearInterval(timer);
+            index--;
+            if(index<0){
+                index=$liImg.length-1;
+            }
+            $liImg.eq(index).fadeIn(500).siblings('li').fadeOut(500);
+            $liNav.eq(index).addClass('active').siblings('li').removeClass('active');
+        })
+        $btnPrev.mouseup(function(){
+            timer=setInterval(autoFocus,iSpeed);
+        })
         function autoFocus(){
             index++;
             if(index>$liImg.length-1){index=0;}
@@ -41,7 +92,9 @@ $(function(){
         }
         timer=setInterval(autoFocus,iSpeed);
     })();
-    //����
+
+
+    //
     (function(){
         $('.shop-sort .link2 dt').click(function(){
             $(this).toggleClass('active').siblings('dd').stop().slideToggle('fast');
@@ -60,27 +113,42 @@ $(function(){
             $(this).addClass('active').siblings('li').removeClass('active');
         });
         //收藏
-        //var oBiao=$('.preview .collect .biao');
-        //oBiao.hover(function(){
-        //    $(this).find('.icon').addClass('icon-hover');
-        //},function(){
-        //    $(this).find('.icon').removeClass('icon-hover');
-        //});
-        //oBiao.click(function(){
-        //    var oCllect=$(this).parents('.collect');
-        //    $(this).find('.icon').toggleClass('icon-active');
-        //    oCllect.find('span.hover').remove();
-        //    if(oCllect.find('span').is('.active')){
-        //        oCllect.find('span.active').remove();
-        //        oCllect.append('<span class="txt hover">添加收藏</span>');
-        //    }else{
-        //        oCllect.append('<span class="txt active">取消收藏</span>');
-        //    }
-        //})
-        //
+        var oBiao=$('.preview .collect .biao');
+        oBiao.hover(function(){
+            var $span=$(this).siblings('span');
+            $span.text('添加收藏');
+            if($span.is('.active')){
+                $span.text('取消收藏');
+            }
+            if($span.is('.hover')){
+                $span.text('添加收藏');
+            }
+        })
 
+        oBiao.find('.icon').toggle(
+            function(){
+                var $span=$(this).parents('.biao').siblings('span');
+                $(this).addClass('icon-active');
+                $span.attr('class','txt active').text('取消收藏');
+            },
+            function(){
+                var $span=$(this).parents('.biao').siblings('span');
+                $(this).removeClass('icon-active');
+                $span.attr('class','txt hover').text('添加收藏');
+            }
+        );
+        //
+        $(window).scroll(function(){
+            var d=$(document).scrollTop();
+            if(d>900){
+                $('.tab-wrap').parents('.wh').addClass('fixed-tab');
+            }else{
+                $('.tab-wrap').parents('.wh').removeClass('fixed-tab');
+            }
+        });
+        //
         $('.product-intro .detail .part:last').css({'border':'0'});
-        //���
+        //
         $('.tab-items li a').click(function(){
             var pos=$($(this).attr('href')).offset().top;
             $("html,body").animate({scrollTop:pos},500);
@@ -90,14 +158,21 @@ $(function(){
         $('.itemInfo .ul-list li').click(function(){
             $(this).addClass('active');
         })
-        //计算轮播图===图片大小进行定位
-        //var bigGlassli=$('.product-intro .itemInfo .preview .slider-main li');
-        //var bigGlassliImg=bigGlassli.find('img');
-        //bigGlassli.find('.zoom-section').css({'margin-left':-bigGlassliImg.width()/2+'px'})
-        //bigGlassli.find('.mousetrap').css({'width':'800px'})
+
+        $(".homenew .deduce .list-bottom li .mask,.homenew .max-cat .category .cat-right li .mask,.classify .items .img-small a .mask").parents("a").hover(function(){
+            $(this).animate({"opacity":"0.6"},200);
+        },function(){
+            $(this).animate({"opacity":"1"},200);
+        });
+
+
+
+
+
+
 
     })();
-    //�
+    //
     (function(){
         $('#protocol').click(function(){
             $('.thickframe').show();
@@ -108,7 +183,7 @@ $(function(){
             return false;
         })
     })();
-    //�
+    //
     (function(){
         $('#orderNum .item:last tr').css({'border':'0'});
         $('.clearing-site span a').click(function(){
@@ -126,7 +201,7 @@ $(function(){
             return false;
         })
     })();
-    //�ջ���ַ
+    //
     (function(){
         $('.my-order .extra-r>a').click(function(){
             $(this).siblings('.active-pop').show();
@@ -136,7 +211,7 @@ $(function(){
             return false;
         })
     })();
-    //��Ʒ����
+    //
     (function(){
         $('.nav-item .title').click(function(){
             $(this).find('.icon').toggleClass('icon-active');
@@ -144,9 +219,8 @@ $(function(){
             $(this).parents('.nav-item').find('.ul-list')
         })
     })();
-    //�Ҳ�������
+    //
     (function(){
-
         $(window).scroll(function(){
             var _top=$(window).scrollTop();
             var btnTop=$('.scroll-bar-top');
@@ -155,28 +229,186 @@ $(function(){
             }else{
                 btnTop.fadeOut();
             }
-
-            if(_top>$(document).height()-900){
-
-                $('.scroll-bar').css({'bottom':'200px'})
-            }
-
-
-            btnTop.click(function(){
-                $('body').stop(true).animate({'scrollTop':'0'},300);
+            btnTop.bind('click',function(){
+                $('html,body').stop(true).animate({'scrollTop':'0'},300);
             })
         })
     })();
 
+    //商品详情评价2015116图片
+    (function(){
+        $('.dis-pic .dis-ol .one-li').click(function(){
+            var index = $(this).index()
+            $(this).addClass('active').siblings().removeClass('active');
+            $(this).parents('.dis-ol').siblings('.box-pic').find('.list-pic').eq(index).show().siblings().hide();
+        })
+    });
     //购物车
     (function(){
-        $('#cart-coupon').find('.btn-coupon').click(function(){
+        //优惠券
+        $('.btn-coupon').click(function(){
+            $('.header,.footer').css('z-index','-1');
             $(this).toggleClass('active');
             $(this).siblings('.ul-list').slideToggle('fast');
             return false;
-        })
+        });
     })();
 
+    //010110结算页-计数
+    (function(){
+        //输入框计数
+        var textareaText=$('.my-clearing .page-leaveword input');
+        textareaText.keydown(function(){
+            var aNum=45,
+                curLength=$(this).val().length,
+                txtNmu=$(this).siblings('span');
+            if(curLength>=aNum){
+                var num=$(this).val().substr(0,aNum-1);
+                $(this).val(num);
+            }
+            else{
+                txtNmu.text((aNum-1)-$(this).val().length+'/45')
+            }
+        });
+    })();
+
+
+    //二期---首页
+    (function(){
+        //新导航
+        var allSort=$('.nav-new .cate .allsort');
+        allSort.find('.item').hover(function(){
+            $(this).find('h3').addClass('hover');
+            $(this).find('.i-mc').stop(true,true).fadeIn(10);
+        },function(){
+            $(this).find('h3').removeClass('hover');
+            $(this).find('.i-mc').fadeOut(10);
+        });
+
+        //新导航分类展开收起
+        var navList=$('.nav-new-list');
+        navList.find('h2').bind('click',function(){
+            navList.find('.allsort').slideToggle(100);
+        });
+
+        //轮播图
+        (function(){
+            var $focus=$('.focus-new');
+            var $btnPrev=$focus.find('.btn-prev');
+            var $btnNext=$focus.find('.btn-next');
+            var $sliderMain=$focus.find('.slider-main');
+            var $liImg=$sliderMain.find('li');
+            var $liNav=$focus.find('.slider-nav').find('li');
+            var index=0;
+            var timer=null;
+            var iSpeed=3000;
+            $liNav.mousedown(function(){
+                clearInterval(timer);
+                index=$(this).index();
+                $liImg.eq(index).fadeIn(500).siblings('li').fadeOut(500);
+                $(this).addClass('active').siblings('li').removeClass('active');
+            });
+            $liNav.mouseup(function(){
+                timer=setInterval(autoFocus,iSpeed);
+            })
+            $sliderMain.hover(function(){
+                clearInterval(timer);
+            },function(){
+                timer=setInterval(autoFocus,iSpeed)
+            })
+
+            $focus.find('.hd').hover(function(){
+                $btnNext.stop(true,true).animate({'right':'0'},100);
+                $btnPrev.stop(true,true).animate({'left':'180px'},100);
+            },function(){
+                $btnNext.stop(true,true).animate({'right':'-134px'},100);
+                $btnPrev.stop(true,true).animate({'left':'-45px'},100);
+            })
+            //下一页
+            $btnNext.mousedown(function(){
+                clearInterval(timer);
+                autoFocus();
+            })
+            $btnNext.mouseup(function(){
+                timer=setInterval(autoFocus,iSpeed);
+            })
+            //上一页
+            $btnPrev.mousedown(function(){
+                clearInterval(timer);
+                index--;
+                if(index<0){
+                    index=$liImg.length-1;
+                }
+                $liImg.eq(index).fadeIn(500).siblings('li').fadeOut(500);
+                $liNav.eq(index).addClass('active').siblings('li').removeClass('active');
+            })
+            $btnPrev.mouseup(function(){
+                timer=setInterval(autoFocus,iSpeed);
+            })
+            function autoFocus(){
+                index++;
+                if(index>$liImg.length-1){index=0;}
+                $liImg.eq(index).fadeIn(500).siblings('li').fadeOut(500);
+                $liNav.eq(index).addClass('active').siblings('li').removeClass('active');
+            }
+            timer=setInterval(autoFocus,iSpeed);
+        })();
+
+
+
+
+
+
+
+
+    })();
+    //二期---B020101分类推荐首页
+    (function(){
+        //
+        var shopImg=$('.reco-shop .shop-img'),
+            aLi=shopImg.find('li'),
+            aLiNum=aLi.length,
+            aLiWidth=aLi.eq(0).outerWidth(true),
+            oUl=shopImg.find('ul'),
+            oUlWidth=aLiWidth*aLiNum,
+            index =0,
+            btnPrev=shopImg.find('.btn-prev'),
+            btnNext=shopImg.find('.btn-next');
+
+        oUl.css({'width':oUlWidth+'px'});
+        btnNext.bind('click',function(){
+            if(index<aLiNum-5){
+                index++;
+                oUl.animate({'margin-left':-index*aLiWidth+'px'},300);
+            }
+            btnOpacity();
+        });
+        btnPrev.bind('click',function(){
+            if(index>0){
+                index--;
+                oUl.animate({'margin-left':-index*aLiWidth+'px'},300);
+            }
+            btnOpacity();
+        })
+        //
+        btnOpacity();
+        function btnOpacity(){
+            if(index==0){
+                btnPrev.delay(100).animate({'opacity':'0.2'},10);
+                btnNext.delay(100).animate({'opacity':'1'},10);
+            }else{
+
+            }
+            if(index==aLiNum-5){
+                btnPrev.delay(100).animate({'opacity':'1'},10);
+                btnNext.delay(100).animate({'opacity':'0.2'},10);
+            }
+        };
+
+
+
+
+    })();
 
 
 })
