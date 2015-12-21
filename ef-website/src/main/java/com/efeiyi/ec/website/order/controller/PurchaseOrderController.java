e002b7f057489ab5596fc64d022153cd754704b1
jacsonzhou <742565508@qq.com>
2015-12-21 14:41:09 +0800

    收礼

--- ef-website/src/main/java/com/efeiyi/ec/website/order/controller/PurchaseOrderController.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/order/controller/PurchaseOrderController.java
@@ -118,13 +118,40 @@ public class PurchaseOrderController extends BaseController {
             return false;
         }
     }
+    private static boolean isNotEmojiCharacter(char codePoint)
+    {
+        return (codePoint == 0x0) ||
+                (codePoint == 0x9) ||
+                (codePoint == 0xA) ||
+                (codePoint == 0xD) ||
+                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
+                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
+                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
+    }
 
+    public static String filterEmoji(String source)
+    {
+        int len = source.length();
+        StringBuilder buf = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+        {
+            char codePoint = source.charAt(i);
+            if (isNotEmojiCharacter(codePoint))
+            {
+                buf.append(codePoint);
+            } else{
+                buf.append("");
+            }
+        }
+        return buf.toString();
+    }
     @RequestMapping("/giftBuy/saveOrUpdateGiftMessage.do")
     @ResponseBody
     public String saveOrUpdateGiftMessage(HttpServletRequest request) {
         try {
             String purchaseOrderId = request.getParameter("purchaseOrderId");
             String message = request.getParameter("giftMessage");
+            message = filterEmoji(message).trim();
             PurchaseOrderGift purchaseOrderGift = (PurchaseOrderGift) baseManager.getObject(PurchaseOrderGift.class.getName(), purchaseOrderId);
             purchaseOrderGift.setGiftMessage(message);
             baseManager.saveOrUpdate(PurchaseOrderGift.class.getName(), purchaseOrderGift);
