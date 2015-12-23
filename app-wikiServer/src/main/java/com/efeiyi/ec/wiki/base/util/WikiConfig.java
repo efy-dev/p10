992798ae0ddf8739d07756c7b46027f0363a308e
kaysonyang <kayson_yang@163.com>
2015-12-23 09:48:02 +0800

    app 服务端  add by kayson

---
+++ app-wikiServer/src/main/java/com/efeiyi/ec/wiki/base/util/WikiConfig.java
@@ -0,0 +1,10 @@
+package com.efeiyi.ec.wiki.base.util;
+
+/**
+ * Created by Administrator on 2015/12/23.
+ *
+ */
+public class WikiConfig {
+    public static final String appKey = "BL2QEuXUXNoGbNeHObD4EzlX+KuGc70U";
+    public static final String SQL_USER_GET ="from MyUser WHERE username=? AND status='1'";
+}
