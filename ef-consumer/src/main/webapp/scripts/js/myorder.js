c3e292b0dc48d70cd12588d4cde67995d19a7a86
yhq21 <947219155@qq.com>
2016-01-05 18:22:22 +0800

    收货地址修改

--- ef-consumer/src/main/webapp/scripts/js/myorder.js
+++ ef-consumer/src/main/webapp/scripts/js/myorder.js
@@ -2,6 +2,7 @@ $(function(){
     (function(){
         $('#orderNum .item:last tr').css({'border':'0'});
         $('.clearing-site span .btn-cart-add').click(function(){
+            $('.header-new,.topbar,.nav-new-list,.footernew').css('z-index','-1');
             $(this).siblings('.active-pop').show();
             $('.clase, .sh-bg').click(function(){
                 $(this).parents('.active-pop').hide();
@@ -12,6 +13,7 @@ $(function(){
     //
     (function(){
         $('.my-order .extra-r>a').click(function(){
+            $('.header-new,.topbar,.nav-new-list,.footernew').css('z-index','-1');
             $('.header,.footer').css('z-index','-1');
             $(this).siblings('.active-pop').show();
             $('.my-order .clase, .my-order .sh-bg').click(function(){
@@ -75,12 +77,12 @@ $(function(){
         alert('sadasdsa')
         $(this).parent().find('.common-popup').show()
     })
-    //帮助中心
+    //��������
     $('.help-order .page .left dt').click(function(){
         $(this).toggleClass('active');
         $(this).siblings('dd').slideToggle('fast');
     })
-    //我的卡券
+    //�ҵĿ�ȯ
     $('.my-kj .am-tabs-nav .ul-list li.items').click(function(){
         var amTabPanel=$(this).parents('.ul-list').siblings('.am-tabs-bd');
         var $index=$(this).index();
@@ -88,12 +90,12 @@ $(function(){
         amTabPanel.find('.am-tab-panel').eq($index).show().siblings('.am-tab-panel').hide();
         return false
     })
-    //我的评价弹出
+    //�ҵ����۵���
     $(".evaluation .addbtn").click(function(){
         $(this).parents(".data-list").siblings(".evaluation .addcon").show();
         $(".evaluation .addbtn").hide()
     })
-    //010213卡券弹出
+    //010213��ȯ����
     $('#efeiyi-dh').click(function(){
         $(this).siblings('.ytacu').show();
         $('.ytacu .dh .no').click(function(){
@@ -101,7 +103,7 @@ $(function(){
         })
         return false
     })
-    //010213卡券弹出2
+    //010213��ȯ����2
     $('#efeiyi-dh').click(function(){
         $(this).siblings('#ytacu2').show();
         $('.ytacu .cg .btn').click(function(){
@@ -112,7 +114,73 @@ $(function(){
     $('.my-kj .am-tabs-nav .ul-list li.items1').mouseenter(function(){
         $(this).addClass('active').siblings().removeClass('active')
     })
+    /*  var GradList = document.getElementById("quacorgrading").getElementsByTagName("input");*/
+    var GradList = $(".quacorgrading input");
+    for(var i=0;i < GradList.length;i++){
+        GradList[i].onmouseover = function(){
+            for(var Qi=0;Qi<GradList.length;Qi++){
+                GradList[Qi].class.backgroundPosition = '-36px -21px';
+            }
+            for(var Qii=0;Qii<this.name;Qii++){
+                GradList[Qii].style.backgroundPosition = '-5px -21px';
+            }
+        }
+    }
+    $(function(){
+        var dlwidth = $(".recommend .carousel .car-content").outerWidth(true);
+        var dllist = $(".recommend .carousel dl").size();
+        var nuke = 0;
 
-
-
+        $("#a_car-right").click(function(){
+            $("#a_car-left").removeClass("hover")
+            if(nuke>dllist-5){
+                nuke=dllist-5;
+                $(this).addClass("hover")
+            }else{
+                nuke++;
+                $(".recommend .carousel .detall").stop().animate({"left":-nuke*dlwidth},500)
+            }
+        })
+        $("#a_car-left").click(function(){
+            $("#a_car-right").removeClass("hover")
+            if(nuke<=0){
+                nuke=0;
+                $(this).addClass("hover")
+            }else{
+                nuke--;
+                $(".recommend .carousel .detall").stop().animate({"left":-nuke*dlwidth},500)
+            }
+        })
+    })
+    //�ҵ�yifeiyi���̺���Ʒ�ղ�
+    $(".efeiyi-collect .efeiyi-head .efeiyi-right li").click(function(){
+        var _myindex = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".efeiyi-col-box .efeiyi-col-tab").eq(_myindex).show().siblings().hide()
+    })
+    //�ҵ�yifeiyi���̺���Ʒ�ղض���1
+    var lilist = $(".efeiyi-col-box .add1-tab .efeiyi-col-tzoom .efeiyi-tzoom-list ul li").size()
+    var liwidth = $(".efeiyi-col-box .efeiyi-col-tab .efeiyi-col-tzoom .efeiyi-tzoom-list ul li").outerWidth(true);
+    var dhul = $(".efeiyi-col-box .efeiyi-col-tab .efeiyi-col-tzoom .efeiyi-tzoom-list ul")
+    var linull = 0;
+    $(".efeiyi-col-box .add-rigt").click(function(){
+        $(".efeiyi-col-box .add-left").removeClass("add")
+        if(linull>=lilist-4){
+            $(this).addClass("add")
+            linull=lilist-4;
+        }else{
+            linull++;
+            dhul.animate({"left":-linull*liwidth},500)
+        }
+    })
+    $(".efeiyi-col-box .add-left").click(function(){
+        $(".efeiyi-col-box .add-rigt").removeClass("add")
+        if(linull<=0){
+            $(this).addClass("add")
+            linull=0;
+        }else{
+            linull--;
+            dhul.animate({"left":-linull*liwidth},500)
+        }
+    })
 })
