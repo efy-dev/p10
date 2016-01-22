483ed335a6cf110712a000cbca21504ac1c280b1
lylylyly <360682644@qq.com>
2016-01-22 13:07:42 +0800

    返利计划统计来源点击量功能，修复404提示性错误

--- ef-website/src/main/java/com/efeiyi/ec/website/base/controller/HomeController.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/base/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -194,6 +195,7 @@ public class HomeController {
     }
 
     @RequestMapping({"/watchUrlSource.do"})
+    @ResponseBody
     public String watchedUrl(HttpServletRequest request) throws Exception {
         String currentUrl = request.getParameter("currentUrl");
         if(currentUrl.contains("source")) {
