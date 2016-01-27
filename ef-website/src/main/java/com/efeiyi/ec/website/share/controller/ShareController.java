cf69c88a48fdc6b14380f7313e716ccfaa9093e6
jacsonzhou <742565508@qq.com>
2016-01-27 15:35:43 +0800

    unionid

--- ef-website/src/main/java/com/efeiyi/ec/website/share/controller/ShareController.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/share/controller/ShareController.java
@@ -72,7 +72,7 @@ public class ShareController {
     @RequestMapping({"/sharePage/openShare/{purchaseOrderId}"})
     public String openPage(@PathVariable String purchaseOrderId,HttpServletRequest request,Model model) throws Exception{
         String callback = URLEncoder.encode("www2.efeiyi.com/sharePage/saveWXInfo.do?purchaseOrderId="+purchaseOrderId,"UTF-8");
-        return "redirect:http://www.efeiyi.com/wx/getUserBaseInfo.do?dataKey=nickname;headimgurl"+"&callback="+callback;
+        return "redirect:http://www.efeiyi.com/wx/getUserBaseInfo.do?dataKey=nickname;unionid;headimgurl"+"&callback="+callback;
     }
     /**分享后返利
      * @param request
