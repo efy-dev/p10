15b0e8a40c2d02557fb03cc58255dd154a9ab8d6
notbadwt <notbad_wangt@126.com>
2016-03-02 14:44:54 +0800

    精简个人中心代码

--- ef-website/src/main/java/com/efeiyi/ec/website/organization/service/UserManager.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/organization/service/UserManager.java
@@ -1,9 +1,13 @@
 package com.efeiyi.ec.website.organization.service;
 
 import com.efeiyi.ec.organization.model.BigUser;
+import com.efeiyi.ec.organization.model.Consumer;
+import com.efeiyi.ec.organization.model.ConsumerAddress;
+import com.efeiyi.ec.organization.model.MyUser;
+import com.ming800.core.does.model.XQuery;
 import com.ming800.core.taglib.PageEntity;
-import com.ming800.core.does.model.PageInfo;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -14,5 +18,27 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public interface UserManager {
+
+    MyUser getUserByUsername(String username);
+
+    MyUser getUserByUserId(String userId);
+
+    Consumer getConsumerByUserId(String userId);
+
+    BigUser getBigUserByUserId(String userId);
+
+    List<ConsumerAddress> listConsumerAddressByUserId(String userId);
+
+    void saveOrUpdateConsumer(Consumer consumer);
+
+    void removeUser(String userId);
+
+    List<Consumer> listConsumer(XQuery xQuery); //需要服务端客户端有相同的配置
+
+    List<Consumer> listConsumer(String hql, LinkedHashMap<String, Object> param);
+
+    List<Consumer> plistConsumer(String queryHql, PageEntity pageEntity, LinkedHashMap<String, Object> queryParamMap);
+
+    List<Consumer> plistConsumer(XQuery xQuery);
 
 }
