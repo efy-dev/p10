a49e8ba2daa8a5f35d90ea394a0377c6c8fc0041
zzchong <zz_chong@163.com>
2016-04-06 11:34:16 +0800

    二维码

--- ef-system/src/main/java/com/efeiyi/ec/system/masterWork/controller/MasterWorkController.java
+++ ef-system/src/main/java/com/efeiyi/ec/system/masterWork/controller/MasterWorkController.java
@@ -1,23 +1,19 @@
 package com.efeiyi.ec.system.masterWork.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.efeiyi.ec.master.model.Master;
-import com.efeiyi.ec.system.master.dao.MasterDao;
-import com.efeiyi.ec.system.master.service.MasterManager;
-import com.ming800.core.base.service.BaseManager;
-import com.ming800.core.does.model.Field;
-import com.ming800.core.does.model.XQuery;
-import com.ming800.core.p.service.AliOssUploadManager;
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +22,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2015/7/20.
@@ -35,27 +34,27 @@ public class MasterWorkController {
 
     HttpClient httpClient = new HttpClient();
     DefaultHttpClient client = new DefaultHttpClient();
-    static transient String accessToken;
+//    static transient String accessToken;
     HttpPost method;
     JSONObject jsonObject = new JSONObject();
     HttpResponse response;
     StringEntity stringEntity;
-    static transient String ticket;
-    public static final String APP_ID = "wx492792c22607523a";
-    public static final String SECRET = "8159d51208af85b5ea13350c74c37ec8";
+//    static transient String ticket;
+    public static final String APP_ID = "wx7f6aa253b75466dd";
+    public static final String SECRET = "04928de13ab23dca159d235ba6dc19ea";
     public static final String GET_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token";// 获取access
 
     //true:永久二维码模式；false：临时二维码模式
 //    private boolean runningModel = false;
-    private boolean runningModel = true;
+    private boolean runningModel = false;
 
-    @RequestMapping(value = "/masterWork/getCode.do")
-    @ResponseBody
+//    @RequestMapping(value = "/masterWork/getCode.do")
+//    @ResponseBody
     public void getCode(HttpServletRequest request) throws Exception {
 
 
             String id = request.getParameter("id");
-            accessToken = getAccessToken(GET_TOKEN_URL,APP_ID,SECRET);
+            String  token = getAccessToken(GET_TOKEN_URL,APP_ID,SECRET);
 
             //永久/临时二维码
             if (runningModel) {
@@ -65,13 +64,13 @@ public class MasterWorkController {
                 //临时
                 prepareTempJsonObject();
             }
-           ticket = getTicket(id);
+           String ticket = getTicket(id,token);
            String root = getClass().getResource("/").getFile().toString()+File.separator+"image";
            getPic(ticket,root);
         }
 
      private String getAccessToken(String apiurl, String appid, String secret){
-
+//         method = new HttpPost(GET_TOKEN_URL+"?grant_type=client_credential&appid=" + appid+"&secret="+secret);
          String turl = String.format("%s?grant_type=client_credential&appid=%s&secret=%s", apiurl,
                                appid, secret);
          HttpGet get = new HttpGet(turl);
@@ -124,7 +123,7 @@ public class MasterWorkController {
             jsonObject.put("action_info", scene);
         }
 
-    @RequestMapping(value = "/masterWork",method = RequestMethod.GET)
+      @RequestMapping(value = "/masterWork/getCode.do",method = RequestMethod.GET)
       public  void  contact(HttpServletRequest request, HttpServletResponse response) throws IOException {
           String s = request.getParameter("echostr");
           System.out.print("echostr ----->"+s);
@@ -153,8 +152,12 @@ public class MasterWorkController {
 //            return "";
 //        }
 
-        private String getTicket(String code) throws IOException {
-            method = new HttpPost("https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=" + accessToken);
+        private String getTicket(String code,String token) throws IOException {
+            String ticket="";
+            DefaultHttpClient client1 = new DefaultHttpClient();
+            method = new HttpPost("https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token="+token);
+//            List<NameValuePair> params = new ArrayList<NameValuePair>();
+//            params.add(new BasicNameValuePair("access_token",token));
             //写入二维码Json参数code
             if (runningModel) {
                 //永久
@@ -163,13 +166,19 @@ public class MasterWorkController {
                 //临时
                 ((JSONObject) ((JSONObject) jsonObject.get("action_info")).get("scene")).put("scene_id", code);
             }
+          try {
+              stringEntity = new StringEntity(jsonObject.toJSONString(), "utf-8");
+              stringEntity.setContentType("application/json");
+              method.setEntity(stringEntity);
+              response = client1.execute(method);
+              String json = EntityUtils.toString(response.getEntity());
+              ticket = (String) JSONObject.parseObject(json).get("ticket");
+          }catch (Exception e){
+              e.printStackTrace();
+          }finally {
+              client1.getConnectionManager().shutdown();
+          }
 
-            stringEntity = new StringEntity(jsonObject.toJSONString(), "utf-8");
-            stringEntity.setContentType("application/json");
-            method.setEntity(stringEntity);
-            response = client.execute(method);
-            String json = EntityUtils.toString(response.getEntity());
-            String ticket = (String) JSONObject.parseObject(json).get("ticket");
 //            if (ticket == null) {
 //                Code2UrlConsumer.accessToken = getAccessToken();
 //                ticket = getTicket(code);
@@ -182,7 +191,7 @@ public class MasterWorkController {
             if (runningModel) {
                 ((JSONObject) ((JSONObject) jsonObject.get("action_info")).get("scene")).put("scene_str", code);
             } else {
-                ((JSONObject) ((JSONObject) jsonObject.get("action_info")).get("scene")).put("scene_id", code);
+                ((JSONObject) ((JSONObject) jsonObject.get("action_info")).get("scene")).put("scene_id", 123);
             }
             stringEntity = new StringEntity(jsonObject.toJSONString(), "utf-8");
             stringEntity.setContentType("application/json");
@@ -204,8 +213,36 @@ public class MasterWorkController {
             return url;
         }
 
+        private  void  downPic(String url){
+            try {
+
+            // 创建流
+            BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+
+            // 生成图片名
+            int index = url.lastIndexOf("/");
+            String sName = url.substring(index+1, url.length());
+            System.out.println(sName);
+            // 存放地址
+            File img = new File("c:"+File.separator+"image");
+            // 生成图片
+            BufferedOutputStream out = new BufferedOutputStream(
+                    new FileOutputStream(img));
+            byte[] buf = new byte[2048];
+            int length = in.read(buf);
+            while (length != -1) {
+                out.write(buf, 0, length);
+                length = in.read(buf);
+            }
+            in.close();
+            out.close();
+        } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
         private void getPic(String ticket,String root) throws IOException {
-            File file = new File(File.separator+"image");
+            File file = new File(root);
             HttpMethod method = new GetMethod("https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" + ticket);
             httpClient.executeMethod(method);
             InputStream is = method.getResponseBodyAsStream();
