d0b82e04057e7a3dff5d6e74bd6f17db41de0c0d
zzchong <zz_chong@163.com>
2016-04-07 15:02:32 +0800

    二维码

--- ef-system/src/main/java/com/efeiyi/ec/system/masterWork/controller/MasterWorkController.java
+++ ef-system/src/main/java/com/efeiyi/ec/system/masterWork/controller/MasterWorkController.java
@@ -345,10 +345,10 @@ public class MasterWorkController {
 //        document.setXMLEncoding("utf-8");
         Element root = document.addElement("xml");
 
-        root.addElement("ToUserName").setText(toUserName);//接收方账号(OpenI)
-        root.addElement("FromUserName").setText(fromUserName);//开发者微信号
+        root.addElement("ToUserName").add(DocumentHelper.createCDATA(toUserName));//接收方账号(OpenI)
+        root.addElement("FromUserName").add(DocumentHelper.createCDATA(fromUserName));//开发者微信号
         root.addElement("CreateTime").setText(Long.toString(System.currentTimeMillis()));//时间
-        root.addElement("MsgType").setText("news");//类型
+        root.addElement("MsgType").add(DocumentHelper.createCDATA("news"));//类型
         root.addElement("ArticleCount").setText("1");//图文个数
         Element item = root.addElement("Articles").addElement("item");//第一个为大图
         item.addElement("Title").add(DocumentHelper.createCDATA(title));
