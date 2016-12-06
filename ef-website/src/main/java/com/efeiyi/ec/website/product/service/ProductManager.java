e9a5dd03ebaf9f9edcf96743ef575ffba7d02ba7
mujianping <18612394628@163.com>
2016-12-06 16:57:51 +0800

    解决冲突

--- ef-website/src/main/java/com/efeiyi/ec/website/product/service/ProductManager.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/product/service/ProductManager.java
@@ -3,6 +3,7 @@ package com.efeiyi.ec.website.product.service;
 import com.efeiyi.ec.product.model.ProductModel;
 import com.efeiyi.ec.purchase.model.CartProduct;
 import com.efeiyi.ec.website.base.util.ApplicationException;
+import com.efeiyi.ec.website.product.model.RequestParamModel;
 
 /**
  * Created by Administrator on 2016/12/5 0005.
@@ -10,7 +11,6 @@ import com.efeiyi.ec.website.base.util.ApplicationException;
 public interface ProductManager {
 
     ProductModel getProductModelById(String id) throws ApplicationException;
-
 
     CartProduct saveCartProduct(ProductModel productModel, Integer amount) throws ApplicationException;
 
